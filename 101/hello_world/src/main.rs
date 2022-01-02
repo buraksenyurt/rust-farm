d5bbda1a632e676dd97125ed65908b035f4712af
buraksenyurt <burakselimsenyurt@gmail.com>
2022-01-02 21:59:14 +0300

    println! samples added.

--- 101/hello_world/src/main.rs
+++ 101/hello_world/src/main.rs
@@ -1,13 +1,13 @@
 /*
-    Hello World
+   Hello World
 
-    Program terminalden girdi alır ekrana basar.
+   Program terminalden girdi alır ekrana basar.
 
-    Çalıştırılabilir programların giriş noktası main fonksiyonudur.
-    Rust dilinde tüm değişkenler varsayılan olarak Immutable'dır. mut ile değiştirilebilir olurlar.
-    match ifadesi ile bir değerin tüm olası koşulları kontrol edilebilir.
-
- */
+   Çalıştırılabilir programların giriş noktası main fonksiyonudur.
+   Rust dilinde tüm değişkenler varsayılan olarak Immutable'dır. mut ile değiştirilebilir olurlar.
+   match ifadesi ile bir değerin tüm olası koşulları kontrol edilebilir.
+    println!, en sık kullanılan macro fonksiyonlarındandır.
+*/
 
 // io kütüphanesini bildirimi
 use std::io;
@@ -24,7 +24,36 @@ fn main() {
     // read_line çıktısı Result<T,Error> tipindendir
     // match ile Ok ve Err halleri kontrol edilir
     match io::stdin().read_line(&mut user_name) {
-        Ok(_) => println!("Merhaba, {}Rust çiftliğine hoş geldin.", user_name.to_uppercase()),
+        Ok(_) => println!(
+            "Merhaba, {}Rust çiftliğine hoş geldin.",
+            user_name.to_uppercase()
+        ),
         Err(e) => println!("Bir şeyler ters gitmiş olabilir. {}", e),
     };
+
+    // println! macro için başka kullanımlar
+
+    // {} ile metin içerisine argüman yerleştirme
+    println!("Ben {} ve {} yaşındayım.", "Burak Selim", 45);
+    // argümanda matematiksel işlem kullanımı
+    println!("3.14 * 10 * 10 = {}", 3.14 * 10.0 * 10.0);
+    // {} argümanını sayılar ile işaretleyip metin içinde farklı konumlarda kullanabilir, tekrar ettirebiliriz.
+    println!(
+        "{0}, {2} elektronik cihaza sahip.\n Bir tanesi {3}. {0}'ın ayrıca {1}'ı da var.",
+        "Burak", "VR", 2, "Desktop PC"
+    );
+    // Hexadecimal, binary ve octal gösterimler
+    println!(
+        "314 sayısının Hex değeri {:x}\nBinary değeri {:b}\nOctal sayı sistemindeki değeri {:o}",
+        314, 314, 314
+    );
+    // Bazen argümanın metinsel gösterimi için çalışma zamanının varsayılan davranışı kullanılabilir (Debug trait kullanımı)
+    println!("Points: {:?}", [3, 5, 7, 9, 11]);
+
+    // metinsel ifadedeki argümanlara isim verilebilir
+    println!(
+        "İlk adım {first_name}.Soyadım {second_name}",
+        first_name = "Burak Selim",
+        second_name = "Senyurt"
+    );
 }

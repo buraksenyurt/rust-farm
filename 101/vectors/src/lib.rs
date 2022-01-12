a44a605f2d80d840eb8cb64d1f28ce86a41bfcf9
buraksenyurt <burakselimsenyurt@gmail.com>
2022-01-12 15:19:38 +0300

    some tests

--- 101/vectors/src/lib.rs
+++ 101/vectors/src/lib.rs
@@ -14,4 +14,24 @@ mod tests {
         // Bu nedenle eleman sayısı 1 azalır
         assert_eq!(points.len(), 2);
     }
+
+    #[test]
+    fn create_vector_with_macro_test() {
+        // Bir vector pratik olarak vec! makrosu ile de oluşturulabilir
+        let mut numbers = vec![5, 3, 9, 2, -12, -14, 7];
+        assert_eq!(numbers.len(), 7);
+        numbers.remove(3);
+        numbers.remove(0);
+        assert_eq!(numbers, [3, 9, -12, -14, 7]);
+        assert_eq!(numbers.len(), 5);
+    }
+
+    #[test]
+    fn change_vector_elements_test() {
+        // 10 tane 1 rakamından oluşan bir vector tanımı
+        let mut numbers = vec![1; 5];
+        // for_each ile var olan vector içindeki elemanları değiştiriyoruz.
+        numbers.iter_mut().for_each(|n| *n += 1);
+        assert_eq!(numbers[2], 2);
+    }
 }

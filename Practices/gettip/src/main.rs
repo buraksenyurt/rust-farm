1a81d266be9a3ebfa01603df2bbd93371926675d
buraksenyurt <burakselimsenyurt@gmail.com>
2022-01-14 22:17:26 +0300

    add some funcs

--- Practices/gettip/src/main.rs
+++ Practices/gettip/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     match args.len() {
         2 => {
             let command = &args[1];
-            println!("Girilen komut -> {}",command);
+            println!("Girilen komut -> {}", command);
         }
         3 => {
             let category = &args[2];
@@ -16,4 +16,22 @@ fn main() {
             println!("Lütfen kullanım klavuzunu okuyun");
         }
     };
+}
+
+fn load_tips() -> &'static Vec<Tip> {
+    todo!()
+}
+
+fn get_random_tip(tips: &Vec<Tip>) -> String {
+    todo!()
+}
+
+fn get_tips_by_category(tips: &Vec<Tip>, category: String) -> String {
+    todo!()
+}
+
+struct Tip {
+    id: i32,
+    category: String,
+    description: String,
 }

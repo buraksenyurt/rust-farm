37efd8934b3708a4cb0358916b061c6a3ab1794c
buraksenyurt <burakselimsenyurt@gmail.com>
2022-01-14 13:44:20 +0300

    add some test

--- 101/structs/src/lib.rs
+++ 101/structs/src/lib.rs
@@ -2,11 +2,41 @@ use std::fmt::{Display, Formatter};
 
 #[cfg(test)]
 mod tests {
+    use crate::Employee;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn create_employee_object_test() {
+        let betty = Employee {
+            id: 1,
+            name: String::from("beti deyvis"),
+            level: 90,
+            company: String::from("Wornır buros"),
+        };
+        assert_eq!(betty.level, 90);
+        assert_eq!(betty.name, "beti deyvis");
+
+        let sean = Employee::new(2, String::from("şan kanıri"), 100, String::from("MGM"));
+        assert_eq!(sean.company, "MGM");
     }
+
+    #[test]
+    fn change_employee_level_test() {
+        let mut arni = Employee {
+            id: 1,
+            name: String::from("arnıld şıvatzenegır"),
+            level: 68,
+            company: String::from("Soni"),
+        };
+        assert_eq!(arni.level, 68);
+        arni.change_level(77);
+        assert_eq!(arni.level, 77);
+    }
+
+    #[test]
+    fn display_behavior_test() {}
+
+    #[test]
+    fn create_vec_of_employees() {}
 }
 
 // Bir çalışanın birkaç bilgisini tutacak türden bir veri yapısı tanımladık
@@ -20,7 +50,7 @@ pub struct Employee {
 // Veri yapımız için birkaç fonksiyon bildirimi
 impl Employee {
     // Yeni bir employee nesnesi oluşturmak için kullanabiliriz.
-    fn new(id: i32, name: String, level: u8, company: String) -> Employee {
+    pub fn new(id: i32, name: String, level: u8, company: String) -> Employee {
         // argüman adları Employee struct'ınınkilerle aynı olduğundan
         // id:e_id gibi bir atama yapmaya gerek kalmaz
         Employee {
@@ -33,7 +63,7 @@ impl Employee {
 
     // O nesnenin level değerini değiştiren örnek fonksiyon
     // self ile çalışma zamanı nesnesinin kendisi ifade edilir.
-    fn change_level(&mut self, new_level: u8) {
+    pub fn change_level(&mut self, new_level: u8) {
         self.level = new_level;
     }
 }

5d57ac89798ef4ca65126d5d6d1b1df54ddb1259
buraksenyurt <burakselimsenyurt@gmail.com>
2022-01-18 17:03:42 +0300

    some tests

--- Practices/uniqueue/src/lib.rs
+++ Practices/uniqueue/src/lib.rs
@@ -5,7 +5,8 @@ mod tests {
     #[test]
     fn empty_uniqueue_test() {
         let qu = Uniqueue::<u8>::new();
-        assert_eq!(qu.items.len(), 0);
+        assert_eq!(qu.count(), 0);
+        assert_eq!(qu.is_empty(), true);
     }
 }
 
@@ -20,5 +21,21 @@ impl<T: Clone> Uniqueue<T> {
     /// Boş bir Uniqueue oluşturur
     pub fn new() -> Uniqueue<T> {
         Uniqueue { items: Vec::new() }
+    }
+
+    /// İçerideki eleman sayısını verir
+    pub fn count(&self) -> usize {
+        self.items.len()
+    }
+
+    /// Kuyruğun boş olup olmadığını söyler
+    pub fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+}
+// clippy önerisiyle eklendi
+impl<T: Clone> Default for Uniqueue<T> {
+    fn default() -> Self {
+        Self::new()
     }
 }

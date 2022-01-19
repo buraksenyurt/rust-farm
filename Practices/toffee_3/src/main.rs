2daa50779720ee759e9d40ded0562c9a44545732
buraksenyurt <burakselimsenyurt@gmail.com>
2022-01-19 23:41:46 +0300

    error handling sample

---
+++ Practices/toffee_3/src/main.rs
@@ -0,0 +1,48 @@
+use std::error::Error;
+use std::fmt::{Display, Formatter};
+
+// non_exhaustive // Gelecekte aşağıdaki enum türüne yeni alanlar eklenebileceğini ifade eder.
+// Geriye uyumluluk adına kullanılması tavsiye edilir.
+
+#[derive(Debug)]
+#[non_exhaustive]
+pub enum CommError {
+    NotConnect(u8),
+    MissingSatellite(String),
+    SunStorm,
+}
+
+impl Error for CommError {}
+
+impl Display for CommError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        use CommError::*;
+
+        match self {
+            NotConnect(t) => write!(f, "{} içerisinde bağlantı sağlanamadı.", t),
+            MissingSatellite(h) => write!(f, "{}, geçerli bir uydu değil.", h),
+            SunStorm => write!(f, "Güneş fırtınası nedeniyle iletişim arızası"),
+        }
+    }
+}
+
+fn main() {
+    let status = connect(String::from("tokyo#1234")).unwrap();
+    println!("Tokyo#1234 sunucusu ile bağlantı {}", status);
+
+    let status = connect(String::from("yucin#5151"));
+    match status {
+        Ok(_) => println!("Bağlantı aktif"),
+        Err(e) => println!("{}", e),
+    };
+
+    println!("İletişim Sonu");
+}
+
+fn connect(satellite_name: String) -> Result<bool, CommError> {
+    if satellite_name.contains("yucin") {
+        Err(CommError::MissingSatellite(satellite_name))
+    } else {
+        Ok(true)
+    }
+}

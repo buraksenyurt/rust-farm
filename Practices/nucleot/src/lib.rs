921f75f1929475561ceaac356bf9367d74858ddc
buraksenyurt <burakselimsenyurt@gmail.com>
2022-01-21 19:23:30 +0300

    first try

--- Practices/nucleot/src/lib.rs
+++ Practices/nucleot/src/lib.rs
@@ -8,13 +8,49 @@ mod tests {
 
     #[test]
     fn valid_dna_sequence_test() {
-        let result = check_dna("GATTACA").unwrap();
-        assert_eq!(result, "başarılı");
+        let result = check_dna("GatTAcA").unwrap();
+        assert_eq!(result.adenine, 3);
+        assert_eq!(result.cytosine, 1);
+        assert_eq!(result.guanine, 1);
+        assert_eq!(result.thymine, 2);
+    }
+
+    #[test]
+    fn invalid_dna_sequence_test() {
+        let result = check_dna("INVALID");
+        match result {
+            Err(e) => assert_eq!(e, DnaError::Invalid),
+            Ok(_) => {}
+        };
     }
 }
 
-pub fn check_dna(sequence: &str) -> Result<&str, DnaError> {
-    Ok("başarılı")
+pub fn check_dna(sequence: &str) -> Result<Nucleotid, DnaError> {
+    let mut is_invalid_char = false;
+    for c in sequence.to_uppercase().chars() {
+        match c {
+            'A' | 'G' | 'T' | 'C' => continue,
+            _ => is_invalid_char = true,
+        }
+    }
+
+    if is_invalid_char {
+        Err(DnaError::Invalid)
+    } else {
+        Ok(Nucleotid {
+            adenine: 3,
+            cytosine: 1,
+            guanine: 1,
+            thymine: 2,
+        })
+    }
+}
+
+pub struct Nucleotid {
+    pub adenine: u8,
+    pub cytosine: u8,
+    pub guanine: u8,
+    pub thymine: u8,
 }
 
 impl Error for DnaError {}
@@ -27,7 +63,8 @@ impl Display for DnaError {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
+#[non_exhaustive]
 pub enum DnaError {
     Invalid,
 }

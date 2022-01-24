36e920b1171e7cb9d60c6d738c080c02c420e434
buraksenyurt <burakselimsenyurt@gmail.com>
2022-01-24 22:04:57 +0300

    armstrong number fn

--- Practices/toffee_4/src/lib.rs
+++ Practices/toffee_4/src/lib.rs
@@ -4,11 +4,24 @@ mod tests {
 
     #[test]
     fn armstrong_numbers_test() {
-        let result = are_you_armstrong_number(154);
-        assert_eq!(result, false);
+        assert_eq!(are_you_armstrong_number(9), true);
+        assert_eq!(are_you_armstrong_number(10), false);
+        assert_eq!(are_you_armstrong_number(153), true);
+        assert_eq!(are_you_armstrong_number(154), false);
+        assert_eq!(are_you_armstrong_number(371), true);
+        assert_eq!(are_you_armstrong_number(999), false);
     }
 }
 
-pub fn are_you_armstrong_number(number: u8) -> bool {
-    false
+pub fn are_you_armstrong_number(number: u32) -> bool {
+    // İlk olarak sayının rakamlarını çekmek isterim.
+    // 10 düzende to_digit fonksiyonu iş görecektir
+    let digits: Vec<_> = number
+        .to_string()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
+    let digits_count = digits.len() as u32;
+    let sum_of: u32 = digits.iter().map(|n| n.pow(digits_count)).sum();
+    sum_of == number
 }

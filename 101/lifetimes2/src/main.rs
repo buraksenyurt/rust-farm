fe73f086b27d0472832eb409ec51ebb335e719fe
buraksenyurt <burakselimsenyurt@gmail.com>
2022-01-27 23:00:32 +0300

    scope and lifetimes

--- 101/lifetimes2/src/main.rs
+++ 101/lifetimes2/src/main.rs
@@ -1,6 +1,28 @@
 fn main() {
     let bruce = Actor::new(48, String::from("Bruce Wayne"));
     println!("({}){}", bruce.sys_id, bruce.get_name());
+
+    /*
+        #2
+        Dikkat edilmesi gereken bir durum scope'lar söz konusu olduğunda kendini gösterir.
+        actors_name isimli değşkeni göz önüne alalım.
+        main scope'u içinde String referans olarak tanımlanmış,
+        iç scope'ta jennifer değişkeninin name değerini ödünç almış ve
+        iç scope sonlandıktan sonra kullanılmak istenmiştir.
+        Aslında actors_name'in yaşam süresi, iç scope sonrasına yetmez :D
+
+        borrowed value does not live long enough
+
+    */
+    let actors_name: &String;
+    {
+        let jennifer = Actor::new(32, String::from("Jenifır Lovrıns"));
+        actors_name = jennifer.get_name();
+        //actors_name = bruce.get_name();
+        // Üstteki atamada ise bir sorun olmaz.Nitekim actors_name ile bruce aynı scope seviyesindedir.
+    }
+    // Hatayı alabilmek için actors_name'i bilerek scope dışında kullandık.
+    println!("{}", actors_name);
 }
 
 // Birde lifetime bilgisinin otomatik olarak atandığı haller vardır.
@@ -13,11 +35,19 @@ impl Actor {
     fn new(sys_id: u16, name: String) -> Self {
         Actor { sys_id, name }
     }
-    // Geriye String nesne referansı döndüren get_name metodunu
-    // lifetime parametresini açık bir şekilde belirterek aşağıdaki gibi yazdığımızı düşünelim.
-    // Kod çalışacaktır ancak aşağıdaki gibi açıkça yaşam ömrünü belirtmemiz gerek yoktur.
-    // explicit lifetimes given in parameter types where they could be elided (or replaced with `'_` if needed by type declaration)
-    fn get_name<'a>(&'a self) -> &'a String {
+    /*
+        #1
+        Geriye String nesne referansı döndüren get_name metodunu
+        lifetime parametresini açık bir şekilde belirterek aşağıdaki gibi yazdığımızı düşünelim.
+        Kod çalışacaktır ancak aşağıdaki gibi açıkça yaşam ömrünü belirtmemiz gerek yoktur.
+
+        explicit lifetimes given in parameter types where they could be elided (or replaced with `'_` if needed by type declaration)
+    */
+    // fn get_name<'a>(&'a self) -> &'a String {
+    //     &self.name
+    // }
+
+    fn get_name(&self) -> &String {
         &self.name
     }
 }

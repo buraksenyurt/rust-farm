5ca88fcdbfd3389a129bb608c8f77c9434147433
buraksenyurt <burakselimsenyurt@gmail.com>
2022-01-31 22:37:48 +0300

    error handling

--- 101/err_handling/src/main.rs
+++ 101/err_handling/src/main.rs
@@ -7,7 +7,36 @@ fn main() {
        İlk kod parçasında vecktor nesnesinin olmayan 10ncu indisine ulaşışmaya çalışılıyor.
 
     */
-    let numbers = vec![3, 5, 1, 6, 9, 11];
-    let number = numbers[10];
-    println!("{}", number);
+
+    // index out of bounds: the len is 6 but the index is 10
+    // let numbers = vec![3, 5, 1, 6, 9, 11];
+    // let number = numbers[10];
+    // println!("{}", number);
+
+    // Bazen bilinçli olaraktan da çalışma zamanını panic ile kesmek isteyebiliriz.
+    // panic!("Canım çalışmak istemiyor");
+    // println!("Biraz önce oluşan panic nedeniyle bu satır çalışmayacak");
+
+    let result = divide(6, Some(4));
+    println!("6 / 4 = {}", result);
+
+    // Aşağıdaki kullanımda ise panik oluşacaktır.
+    let result = divide(6, Some(0));
+    println!("6 / 0 = {}", result);
+
+    println!("Yurdaki panic nedeniyle kod buraya gelemeyecek");
+}
+
+/*
+   Aşağıdaki bölme fonksiyonunu göz önüne alalım.
+   Sıfıra bölme işlemi belirsiz kabul edildiğinden bunu istemiyoruz.
+   y değerini Option<i32> olarak alıp durumu match pattern uygulayarak kontrol altına alabiliriz.
+*/
+fn divide(x: i32, y: Option<i32>) -> i32 {
+    // 0 olması halinde panic makrosu ile sistemi kesmekteyiz.
+    match y {
+        Some(0) => panic!("Sıfıra bölme hatası"),
+        Some(v) => x / v,
+        None => 0,
+    }
 }

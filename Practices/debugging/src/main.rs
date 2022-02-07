beb472564cbba1239acef5fbb8ab4b20c1cb4a70
buraksenyurt <burakselimsenyurt@gmail.com>
2022-02-07 23:19:55 +0300

    new fn added for gdb

--- Practices/debugging/src/main.rs
+++ Practices/debugging/src/main.rs
@@ -8,6 +8,8 @@ fn main() {
     dbg!(calderon.level);
     decrease_level(&mut calderon);
     dbg!(calderon.level);
+    change_level(&mut calderon);
+    dbg!(calderon.level);
 }
 
 fn increase_level(p: &mut Player) {
@@ -16,6 +18,11 @@ fn increase_level(p: &mut Player) {
 fn decrease_level(p: &mut Player) {
     let rate = 10;
     p.level -= rate;
+}
+
+fn change_level(p: &mut Player) {
+    let level = Box::new(90);
+    p.level = *level;
 }
 
 #[allow(dead_code)]

439a4cf6dd835ec1bb55e97127a07eb0b3b791d2
buraksenyurt <burakselimsenyurt@gmail.com>
2022-02-15 22:24:58 +0300

    listening address and port

--- Practices/crayz_server/src/lib.rs
+++ Practices/crayz_server/src/lib.rs
@@ -1,7 +1,8 @@
 /// HTTP Sunucu motoru ile ilgili çekirdek fonksiyonellikleri barındırır.
 pub mod server {
+    use log::{error, info};
     use std::fmt::{Display, Formatter};
-    use log::info;
+    use std::net::TcpListener;
 
     /// Sunucu bilgilerini taşıyan veri yapısı.
     pub struct Server {
@@ -16,17 +17,42 @@ pub mod server {
             Server { root, port, alias }
         }
 
+        fn address(&self) -> String {
+            format!("{}:{}", &self.root, &self.port)
+        }
+
         /// Sunucuyu dinleme modunda başlatır
         pub fn run(self) {
             /*
-           Tipik bir HTTP sunucusu başlatıldığında sonsuz bir döngüde talep dinler.
-           Dolayısıyla run fonksiyonu sonlandığında self ile ifade edilen ve
-           sahiliği(ownership)'i alınan Server nesnesinin deallocate edilmesinde yarar vardır.
-           Bu sebepten &self yerine self kullandık.
-        */
-            info!("{} başlatılıyor...", self.to_string());
-            //TODO Listener yazılacak
-            info!("{} başlatıldı...", self.to_string());
+               Tipik bir HTTP sunucusu başlatıldığında sonsuz bir döngüde talep dinler.
+               Dolayısıyla run fonksiyonu sonlandığında self ile ifade edilen ve
+               sahiliği(ownership)'i alınan Server nesnesinin deallocate edilmesinde yarar vardır.
+               Bu sebepten &self yerine self kullandık.
+            */
+            info!("{} başlatılıyor...", self.address());
+            /*
+                Sunucuyu hazırlamak için TcpListener'ın bind fonksiyonu kullanılır.
+            */
+            let listener = TcpListener::bind(&self.address());
+            match listener {
+                Ok(l) => {
+                    info!("{} başlatıldı...", self.to_string());
+                    // Gelen talepleri sonsuz bir döngüde dinleyebiliriz.
+                    loop {
+                        // Gelen yeni bağlantıları match ifadesi ile kontrol altına alıyoruz.
+                        match l.accept() {
+                            Ok((stream, addrees)) => {
+                                info!("{}", addrees.to_string());
+                                //TODO Gelen istekleri kabul eden kod yazılacak
+                            }
+                            Err(e) => error!("{:?}", e),
+                        }
+                    }
+                }
+                Err(e) => {
+                    error!("Sunucu başlatılamadı.{}", e);
+                }
+            }
         }
     }
 
@@ -54,8 +80,8 @@ pub mod http {
 
     /// Request ile ilgili enstrümanları barındırır.
     pub mod request {
-        use std::fmt::{Display, Formatter};
         use super::common::Method;
+        use std::fmt::{Display, Formatter};
         /// HTTP Request içeriğini tutar.
         pub struct Request {
             pub method: Method,
--- Practices/crayz_server/src/main.rs
+++ Practices/crayz_server/src/main.rs
@@ -1,18 +1,21 @@
-use log::info;
-use crayz_server::http::common::Method;
-use crayz_server::http::request::Request;
+// use crayz_server::http::common::Method;
+// use crayz_server::http::request::Request;
 use crayz_server::server::Server;
 
 fn main() {
+    // loglamayı açtık
     env_logger::init();
 
-    let alpha = Server::new("127.0.0.1".to_string(), 8080_u16, "localhost".to_string());
+    // Server veri yapımızı kullanarak bir örnek oluşturduk
+    let alpha = Server::new("0.0.0.0".to_string(), 5555_u16, "localhost".to_string());
+    // run fonksiyonunu çağırıp sunucuyu başlatıyoruz. ya da başlatamıyoruz :)
     alpha.run();
 
-    let get_player = Request {
-        method: Method::Get(Some("?layer>50".to_string())),
-        path: "/player".to_string(),
-    };
 
-    info!("{}", get_player);
+    // let get_player = Request {
+    //     method: Method::Get(Some("?layer>50".to_string())),
+    //     path: "/player".to_string(),
+    // };
+    //
+    // info!("{}", get_player);
 }

96a014c18992271eccd0bf1957925021ae66263b
buraksenyurt <burakselimsenyurt@gmail.com>
2022-02-21 22:00:21 +0300

    lifetimes errors

--- Practices/viva_las_vegas/src/main.rs
+++ Practices/viva_las_vegas/src/main.rs
@@ -3,13 +3,13 @@
 #[derive(Debug)]
 struct Player {
     id: u32,
-    nick: String,
-    country: String,
+    nick: &str,
+    country: &str,
     level: u16,
 }
 
 impl Player {
-    fn new(id: u32, nick: String, country: String, level: u16) -> Self {
+    fn new(id: u32, nick: &str, country: &str, level: u16) -> Self {
         Self {
             id,
             nick,
@@ -19,7 +19,40 @@ impl Player {
     }
 }
 
+// #1nci Durum
+
+// #[derive(Debug)]
+// struct Player {
+//     id: u32,
+//     nick: String,
+//     country: String,
+//     level: u16,
+// }
+
+// impl Player {
+//     fn new(id: u32, nick: String, country: String, level: u16) -> Self {
+//         Self {
+//             id,
+//             nick,
+//             country,
+//             level,
+//         }
+//     }
+// }
+
+
+/*
+    Fonksiyon bir Player değişkeninin referansını almakta ve ikinci parametre bilgisini kullanıp
+    nickname'i değiştirdikten sonra geriye Player referansını iade etmekte.
+ */
+fn change_nickname(p: &mut Player, new_nickname: String) -> &Player {
+    p.nick = new_nickname;
+    p
+}
+
 fn main() {
-    let gonzi = Player::new(1, "Gonsalez".to_string(), "Brasil".to_string(), 88);
+    let mut gonzi = Player::new(1, "Gonsalez".to_string(), "Brasil".to_string(), 88);
     println!("{:#?}", gonzi);
+    let ceremiya = change_nickname(&mut gonzi, "Ceremiya".to_string());
+    println!("{:#?}", ceremiya);
 }

e43652c594374a7976158e281ec1f651de31f4cc
buraksenyurt <burakselimsenyurt@gmail.com>
2022-02-23 22:43:10 +0300

    Pass by ref

--- Practices/by_val_by_ref/src/main.rs
+++ Practices/by_val_by_ref/src/main.rs
@@ -1,26 +1,10 @@
 fn main() {
     let binding = Binding::Https(SoapVersion::V1_2);
-    prepare_env(binding);
-    prepare_env(binding);
+    prepare_env(&binding);
+    prepare_env(&binding);
 }
 
-#[allow(dead_code)]
-#[derive(Debug, Clone, Copy)]
-enum SoapVersion {
-    V1_1,
-    V1_2,
-}
-
-#[allow(dead_code)]
-#[derive(Clone, Copy)]
-enum Binding {
-    Http(SoapVersion),
-    Https(SoapVersion),
-    Rest,
-    Grpc,
-}
-
-fn prepare_env(b: Binding) {
+fn prepare_env(b: &Binding) {
     match b {
         Binding::Http(v) | Binding::Https(v) => {
             println!("HTTP {:?} versiyonu için ortam hazırlanıyor.", v)
@@ -28,4 +12,21 @@ fn prepare_env(b: Binding) {
         Binding::Rest => println!("Servis REST protokolüne göre hazırlanıyor"),
         Binding::Grpc => println!("Servis GRPC protoklüne göre hazırlanıyor"),
     }
+}
+
+#[allow(dead_code)]
+//#[derive(Debug, Clone, Copy)]
+#[derive(Debug)]
+enum SoapVersion {
+    V1_1,
+    V1_2,
+}
+
+#[allow(dead_code)]
+//#[derive(Clone, Copy)]
+enum Binding {
+    Http(SoapVersion),
+    Https(SoapVersion),
+    Rest,
+    Grpc,
 }

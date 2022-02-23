3b4d8f9b3692b4d92e71abd26d77916e0737869e
buraksenyurt <burakselimsenyurt@gmail.com>
2022-02-23 22:27:28 +0300

    Pass by Value

--- Practices/by_val_by_ref/src/main.rs
+++ Practices/by_val_by_ref/src/main.rs
@@ -1,13 +1,18 @@
 fn main() {
     let binding = Binding::Https(SoapVersion::V1_2);
     prepare_env(binding);
+    prepare_env(binding);
 }
 
-#[derive(Debug)]
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy)]
 enum SoapVersion {
     V1_1,
     V1_2,
 }
+
+#[allow(dead_code)]
+#[derive(Clone, Copy)]
 enum Binding {
     Http(SoapVersion),
     Https(SoapVersion),

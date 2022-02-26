37470da00f7818ab5addb667ebbfdcaf50aa168c
buraksenyurt <burakselimsenyurt@gmail.com>
2022-02-26 20:26:37 +0300

    only one mutable #3

--- Practices/only_you/src/main.rs
+++ Practices/only_you/src/main.rs
@@ -1,30 +1,30 @@
+use std::sync::{Arc, Mutex};
 use std::thread;
 
 fn main() {
-    // Birkaç thread'in ortaklaşa kullanmak istediği bir sayı belirleyelim.
-    let mut popular_number = 23_u32;
+    // Birkaç thread'in ortaklaşa kullanmak istediği bir sayı var.
+    // Thread Safe Reference Counter ile birlikte Mutex nesnesi olarak oluşturuyoruz
+    let popular_number = Arc::new(Mutex::new(23_u32));
 
     // JoinHandle nesneleri için bir vector. Main'i bekletmek için kullanırız.
     let mut handlers = Vec::new();
 
     // 10 tane thread açalım
     for _ in 0..10 {
+        // Atomic Reference Counter'ın bir klonunu oluşturuyoruz
+        // Thread'ler bunu kullanacak
+        let number_clone = Arc::clone(&popular_number);
+
         handlers.push(thread::spawn(move || {
+            // klonlanan referans bilgisine bağlı Mutex içeriğini kitliyoruz
+            let mut p = number_clone.lock().unwrap();
 
-            println!(
-                "{:?} Parametre : {}",
-                thread::current().id(),
-                popular_number
-            );
+            println!("{:?} Parametre : {:?}", thread::current().id(), p);
 
-            // Her bir thread popüler olan sayımızı 1 birim artırsın
-            popular_number += 1;
+            // p değerini dereference ettikten sonra 1 artırıyoruz
+            *p += 1;
 
-            println!(
-                "\t{:?} Yeni Değer : {}",
-                thread::current().id(),
-                popular_number
-            );
+            println!("\t{:?} Yeni Değer : {:?}", thread::current().id(), p);
         }));
     }
 
@@ -32,5 +32,14 @@ fn main() {
         let _ = h.join();
     }
 
-    println!("Sayının değeri {}", popular_number);
+    // Thread'lerin ortaklaşa kullandığı sayısal değer aslında Atomic Reference Counter tarafından
+    // tutulan bir Mutext değişkeni. Bir şekilde sonucu almak gerekiyor.
+    // Şimdilik aşağıdaki gibi bir yol bulabildim.
+    println!(
+        "Sayının değeri {}",
+        Arc::try_unwrap(popular_number)
+            .unwrap()
+            .into_inner()
+            .unwrap()
+    );
 }

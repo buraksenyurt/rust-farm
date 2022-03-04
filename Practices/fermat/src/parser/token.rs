ccd25cbb513e231835e62a14499289771e335d9a
buraksenyurt <burakselimsenyurt@gmail.com>
2022-03-05 00:01:52 +0300

    token precedence added

--- Practices/fermat/src/parser/token.rs
+++ Practices/fermat/src/parser/token.rs
@@ -4,6 +4,7 @@
    karşılaştıysak Add, üs alma operatörü ise Caret, ifade sonuna geldiysek EOF,
    bir sayı söz konusu ise Number(f64) vs şeklinde atamalar yapabiliriz.
 */
+
 #[derive(Debug, PartialEq)]
 pub enum Token {
     Add,
@@ -15,4 +16,32 @@ pub enum Token {
     RightBracket,
     Number(f64),
     EndOfExpression,
+}
+
+// Bu enum sabiti ile işlem önceliklerini en düşük olandan en önemli olana doğru sırlamaktayız.
+#[derive(Debug, PartialEq, PartialOrd)]
+pub enum Precedence {
+    Zero,
+    AddSub,
+    MulDiv,
+    Power,
+    Negative,
+}
+
+/*
+   İşlem önceliğini bulmak için aşağıdaki fonksiyondan yararlanabiliriz.
+   Token'ının ne olduğuna bakarak geriye önceliklendirme enum sabitindeki ilgili değeri döndürmekte.
+*/
+impl Token {
+    pub fn get_precedence(&self) -> Precedence {
+        use self::Precedence::*;
+        use self::Token::*;
+
+        match self {
+            Add | Substract => AddSub,
+            Multiply | Divide => MulDiv,
+            Caret => Power,
+            _ => Zero,
+        }
+    }
 }

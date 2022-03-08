7305e816a877b5b731f0ec804e4c9c2e072141f6
buraksenyurt <burakselimsenyurt@gmail.com>
2022-03-08 22:52:20 +0300

    coding

--- Practices/wordle/src/lib.rs
+++ Practices/wordle/src/lib.rs
@@ -1,4 +1,12 @@
-/// Kullanıcının girdiği kelime üstünde bazı düzeltmeler yapan fonksiyon
+/*
+   Kelime listesi sadece okuma amaçlı kullanılacak.
+   include_str! makrosu parametre olarak gelen dosyayı derleme zamanında alıp kaynak kodun içerisine gömer.
+   Dolayısıyla data dosyasını release aldıktan sonra programı götürdüğümüz yere taşımaya gerek yoktur.
+*/
+const WORDS: &str = include_str!("words.data");
+const WORD_LENGTH: usize = 5;
+
+/// Kelime üstünde bazı iyileştirmeler yapan fonksiyondur.
 fn sanitize_word(word: &str) -> String {
     /*
     Bir kaç Higher Order Function kullanarak gelen kelime üstünde işlmeler yapılmakta.
@@ -12,5 +20,19 @@ fn sanitize_word(word: &str) -> String {
         .to_uppercase()
         .chars()
         .filter(|c| c.is_ascii_uppercase())
+        .collect()
+}
+
+/// Kelimeleri String türden vektöre alan fonksiyon
+fn word_list() -> Vec<String> {
+    /*
+    Şimdi elimizde kelimeleri tutan dosya var. Bunu WORDS isimli constant'ta tutuyoruz.
+    Bu dosyadaki herbir satırı okuyup, sanitize işleminden geçirdikten sonra,
+    uzunluğu 5 karakter olanları String türden bir vector'de topluyoruz.
+     */
+    WORDS
+        .split('\n')
+        .map(sanitize_word)
+        .filter(|line| line.len() == WORD_LENGTH)
         .collect()
 }
--- Practices/wordle/src/main.rs
+++ Practices/wordle/src/main.rs
@@ -1,12 +1,5 @@
 use colored::Colorize;
 
-/*
-   Kelime listesi sadece okuma amaçlı kullanılacak.
-   include_str! makrosu parametre olarak gelen dosyayı derleme zamanında alıp kaynak kodun içerisine gömer.
-   Dolayısıyla data dosyasını release aldıktan sonra programı götürdüğümüz yere taşımaya gerek yoktur.
-*/
-const WORDS: &str = include_str!("words.data");
-
 fn main() {
     let wellcome = "World oyununun klonuna hoş geldiniz.".yellow();
     println!("{}", wellcome);

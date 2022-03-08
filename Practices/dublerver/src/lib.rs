5bc3b86fdfd7bb93adb8591dd5ae0e571ce549fc
buraksenyurt <burakselimsenyurt@gmail.com>
2022-03-08 15:15:21 +0300

    TcpListener implementation

--- Practices/dublerver/src/lib.rs
+++ Practices/dublerver/src/lib.rs
@@ -1,4 +1,8 @@
 pub mod server {
+    use log::{error, info};
+    use std::io::{Read, Write};
+    use std::net::TcpListener;
+
     pub struct Server {
         pub alias: String,
         pub address: String,
@@ -16,6 +20,34 @@ pub mod server {
 
         pub fn get_address(&self) -> String {
             format!("{}:{}", self.address, self.port)
+        }
+
+        pub fn run(self) {
+            let listener = TcpListener::bind(&self.get_address());
+            info!("{}\t{} aktif ve dinliyor...", &self.alias, &self.get_address());
+            match listener {
+                Ok(endpoint) => loop {
+                    match endpoint.accept() {
+                        Ok((mut stream, _adddress)) => {
+                            let mut buffer = [0_u8; 512];
+                            if let Ok(length) = stream.read(&mut buffer) {
+                                let msg = String::from_utf8(buffer[0..length].to_vec());
+                                info!("{:?}", msg.unwrap());
+                            }
+                            match write!(stream, "HTTP/1.1 200 Ok \r\n\r\nPONG!\r\n") {
+                                Ok(_) => {}
+                                Err(e) => error!("{}", e),
+                            }
+                        }
+                        Err(e) => {
+                            error!("{}", e)
+                        }
+                    }
+                },
+                Err(e) => {
+                    error!("{}", e);
+                }
+            }
         }
     }
 }

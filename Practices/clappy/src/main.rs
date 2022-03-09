a0591bf8406d937102b2f6817922ea15de61e33f
buraksenyurt <burakselimsenyurt@gmail.com>
2022-03-09 22:11:45 +0300

    some usages

--- Practices/clappy/src/main.rs
+++ Practices/clappy/src/main.rs
@@ -1,27 +1,42 @@
-use clap::{Arg, Command};
+use clap::{arg, Command};
 
 fn main() {
-    let app = Command::new("Crayz Server")
+    // #1 Örnek
+    let matches = Command::new("Crazy Server")
+        .propagate_version(true)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
         .about("Tek çekirdek veya çok çekirdek destekli Web sunucusu.")
         .version("v0.1.0")
         .author("Burak Selim Senyurt")
         .subcommand(
-            Command::new("start")
+            Command::new("run")
                 .about("Web sunucusunu başlatır.")
                 .short_flag('s')
-                .subcommand(
-                    Command::new("mode")
-                        .about("Hangi mod? Tek çekirdek ya da daha fazlası ;)")
-                        .short_flag('m')
-                        .args(&[
-                            Arg::new("one").short('o').help("Tek thread ile başlar."),
-                            Arg::new("parallel")
-                                .short('p')
-                                .help("Çekirdek sayısı kadar thread ile başlar."),
-                        ]),
-                ),
-        );
+                .arg(arg!([MODE] "Hangi modda çalıştırmak istersin?")),
+        )
+        .get_matches();
 
-    let matches = app.get_matches();
-    println!("{:#?}", matches);
+    match matches.subcommand() {
+        Some(("run", argmatchs)) => {
+            let mode = argmatchs
+                .value_of("MODE")
+                .expect("Bir mod girilmemiş olabilir.");
+            println!("{} moduna göre işlem yapılacak.", mode);
+        }
+        _ => {}
+    }
+
+    // #2 Örnek
+    // Bir diğer kullanım şekli
+    // let matches = Command::new("Crazy Server")
+    //     .version("v0.1.0")
+    //     .author("Burak Selim Senyurt")
+    //     .about("Tek çekirdek veya çok çekirdek destekli Web sunucusu.")
+    //     .arg(arg!(-s --start ... "Sunucuyu başlatmak için kullanılır."))
+    //     .arg(arg!(-m --mode <VALUE> "Tek thread veya paralel iki thread.").required(true))
+    //     .get_matches();
+    //
+    // let mode = matches.value_of("mode").unwrap();
+    // println!("Gelen mode bilgisi {}", mode);
 }

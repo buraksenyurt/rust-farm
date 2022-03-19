fd0963d86a7b1362c6ebf4b9ff6472b99ddf1ea0
buraksenyurt <burakselimsenyurt@gmail.com>
2022-03-20 01:15:31 +0300

    Added movement functionality.

--- handson/submarine/src/rock.rs
+++ handson/submarine/src/rock.rs
@@ -1,7 +1,6 @@
-use crate::contant::{SCREEN_HEIGHT, SCREEN_WIDTH};
+use crate::constant::{SCREEN_HEIGHT, SCREEN_WIDTH};
 use crate::player::Player;
-use bracket_lib::prelude::VirtualKeyCode::B;
-use bracket_lib::prelude::{to_cp437, BTerm, RandomNumberGenerator, BLACK, RED};
+use bracket_lib::prelude::{BTerm, RandomNumberGenerator, BLACK, RED};
 
 pub struct Rock {
     pub x: i32,
@@ -14,10 +13,10 @@ impl Rock {
     pub fn new() -> Self {
         let mut random = RandomNumberGenerator::new();
         Self {
-            x: random.range(SCREEN_WIDTH - 20, SCREEN_WIDTH),
-            y: random.range(SCREEN_HEIGHT - 20, SCREEN_HEIGHT),
-            width: random.range(1, 5),
-            height: random.range(1, 5),
+            x: SCREEN_WIDTH,
+            y: random.range(0, SCREEN_HEIGHT - 5),
+            width: 2,
+            height: 2,
         }
     }
 
@@ -26,6 +25,15 @@ impl Rock {
     }
 
     pub fn hit_rock(&self, player: &Player) -> bool {
-        player.x == self.x
+        player.x == self.x && (player.y > self.y - 5 && player.y < self.y + 5)
+    }
+
+    pub fn forward(&mut self, x: i32, y: i32) -> Self {
+        Self {
+            x,
+            y,
+            width: 2,
+            height: 2,
+        }
     }
 }

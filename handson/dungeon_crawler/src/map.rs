14769129536afdbdba15b2105ecfaf390365a1ef
buraksenyurt <burakselimsenyurt@gmail.com>
2022-03-26 15:35:07 +0300

    Added helper functions and tests for x,y coordinates.

--- handson/dungeon_crawler/src/map.rs
+++ handson/dungeon_crawler/src/map.rs
@@ -6,7 +6,7 @@ use crate::prelude::*;
 const NUMBER_OF_TILES: usize = { SCHENE_WIDTH * SCHENE_HEIGHT } as usize;
 
 // Haritadaki hücrelerin tipleri. Duvar olabilir veya bir zemindir.
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq)]
 pub enum TileType {
     Wall,
     Floor,
@@ -41,6 +41,31 @@ impl Map {
             }
         }
     }
+
+    // Bu fonksiyon ile Point ile gelen x,y koordinatlarının oyun sahası içinde olup olmadığı
+    // kontrol ediliyor. Oyuncunun saha içinde olup olmadığını anlamak için eklenmiş olan yardımcı
+    // bir fonksiyon.
+    pub fn is_in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCHENE_WIDTH && point.y >= 0 && point.y < SCHENE_HEIGHT
+    }
+
+    // Bu fonksiyon ile de gelen x,y koordinatlarının oyun sahası içinde ve bir zemin olup
+    // olmadığı kontrol ediliyor. Oyuncunun oyun sahası içinde olması ve
+    // bir duvardan(TileType::Wall) geçmeye çalışıp çalışmadığını kontrol eden yardımcı fonksiyon.
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.is_in_bounds(point) && self.tiles[map_to_index(point.x, point.y)] == TileType::Floor
+    }
+
+    // Bu fonksiyon ile x,y koordinatlarının map vektöründeki bir indise dönüştürülebileceğini
+    // garanti altına almış oluyoruz. Eğer x,y oyun sahası sınırları içinde değilse None dönüyor.
+    // Oyun sahası içindeyse de karşılığı olan vecktor indis değerini çeken fonksiyonu kullanıyor.
+    pub fn try_map_to_index(&self, point: Point) -> Option<usize> {
+        if !self.is_in_bounds(point) {
+            None
+        } else {
+            Some(map_to_index(point.x, point.y))
+        }
+    }
 }
 
 // Bu fonksiyon ile ekrandaki x,y koordinatının Map veri yapısının tuttuğu haritayı temsil eden
@@ -50,4 +75,37 @@ impl Map {
 // Kitapta bunun için Y-First(Row-First) tekniği kullanılmış.
 pub fn map_to_index(x: i32, y: i32) -> usize {
     ((y * SCHENE_WIDTH) + x) as usize
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn should_outside_coordinates_returns_false() {
+        let m = Map::new();
+        let actual = m.is_in_bounds(Point::new(81, 51));
+        assert_eq!(actual, false);
+    }
+
+    #[test]
+    fn should_valid_coordinates_returns_true() {
+        let m = Map::new();
+        let actual = m.is_in_bounds(Point::new(50, 25));
+        assert_eq!(actual, true);
+    }
+
+    #[test]
+    fn should_player_can_enter_tile() {
+        let m = Map::new();
+        let actual = m.can_enter_tile(Point::new(10, 10));
+        assert_eq!(actual, true);
+    }
+
+    #[test]
+    fn should_coordinates_can_convert_to_vector_index() {
+        let m = Map::new();
+        let actual = m.try_map_to_index(Point::new(10, 10));
+        assert_eq!(actual, Some(810));
+    }
 }

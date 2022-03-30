a598941ba6251959cad0a46fbe025d1ed4206145
buraksenyurt <burakselimsenyurt@gmail.com>
2022-03-30 15:33:55 +0300

    Added Enum data structure and a new function for calculating fizzbuzz.

--- Practices/fizz_bench/src/lib.rs
+++ Practices/fizz_bench/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter};
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -13,6 +15,18 @@ mod tests {
         let number = 4;
         assert_eq!(fizz_buzz_v1(number), "4");
     }
+
+    #[test]
+    fn should_number_convertable_to_word_enum() {
+        let number = 3;
+        assert_eq!(fizz_buzz_v2(number), Word::Fizz);
+        let number = 5;
+        assert_eq!(fizz_buzz_v2(number), Word::Buzz);
+        let number = 15;
+        assert_eq!(fizz_buzz_v2(number), Word::FizzBuzz);
+        let number = 4;
+        assert_eq!(fizz_buzz_v2(number), Word::Number(4));
+    }
 }
 
 pub fn fizz_buzz_v1(i: i32) -> String {
@@ -24,5 +38,40 @@ pub fn fizz_buzz_v1(i: i32) -> String {
         "Buzz".to_string()
     } else {
         i.to_string()
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Word {
+    /// 3 ile tam bölünenler
+    Fizz,
+    /// 5 ile tam bölünenler
+    Buzz,
+    /// 15 ile tam bölünenler
+    FizzBuzz,
+    /// Diğer sayılar
+    Number(i32),
+}
+
+impl Display for Word {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match *self {
+            Word::Fizz => f.write_str("Fizz"),
+            Word::Buzz => f.write_str("Buzz"),
+            Word::FizzBuzz => f.write_str("FizzBuzz"),
+            Word::Number(n) => write!(f, "{}", n),
+        }
+    }
+}
+
+pub fn fizz_buzz_v2(number: i32) -> Word {
+    if number % 15 == 0 {
+        Word::FizzBuzz
+    } else if number % 3 == 0 {
+        Word::Fizz
+    } else if number % 5 == 0 {
+        Word::Buzz
+    } else {
+        Word::Number(number)
     }
 }

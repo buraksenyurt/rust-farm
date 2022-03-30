bc0267d80cbe47f6be8f17e4903f4bbbafeaf4a8
buraksenyurt <burakselimsenyurt@gmail.com>
2022-03-30 16:00:39 +0300

    Added the benchmark functions.

--- Practices/fizz_bench/benches/measurements.rs
+++ Practices/fizz_bench/benches/measurements.rs
@@ -1,1 +1,21 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use fizz_bench::*;
 
+pub fn fizz_buzz_enum_benchmark(c: &mut Criterion) {
+    c.bench_function("Fizz Buzz with Enum", |b| {
+        b.iter(|| fizz_buzz_v2(black_box(15)))
+    });
+}
+
+pub fn fizz_buzz_string_benchmark(c: &mut Criterion) {
+    c.bench_function("Fizz Buzz with String", |b| {
+        b.iter(|| fizz_buzz_v1(black_box(15)))
+    });
+}
+
+criterion_group!(
+    benches,
+    fizz_buzz_enum_benchmark,
+    fizz_buzz_string_benchmark
+);
+criterion_main!(benches);

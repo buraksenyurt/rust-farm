e0b000ad8438d5f17933e4436b1e2d74401f94a4
buraksenyurt <burakselimsenyurt@gmail.com>
2022-04-02 13:43:22 +0300

    Added web server to program.

--- WebDev/src/actix_views/src/main.rs
+++ WebDev/src/actix_views/src/main.rs
@@ -1,5 +1,15 @@
+use actix_web::{App, HttpServer};
+
 mod views;
 
-fn main() {
-    println!("Hello, world!");
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    //Standart olarak belli bir ip:port üstünden web sunucusunu başlatıyoruz.
+    HttpServer::new(|| {
+        let app = App::new();
+        return app;
+    })
+    .bind("0.0.0.0:8000")?
+    .run()
+    .await
 }

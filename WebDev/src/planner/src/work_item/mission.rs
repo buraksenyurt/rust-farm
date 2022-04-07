e7863f1a9263faa0abc9713670cd188b197ac200
buraksenyurt <burakselimsenyurt@gmail.com>
2022-04-07 23:49:13 +0300

    Added Display support to Mission struct.

--- WebDev/src/planner/src/work_item/mission.rs
+++ WebDev/src/planner/src/work_item/mission.rs
@@ -1,6 +1,7 @@
 use super::model::completed::Completed;
 use super::model::doing::Doing;
 use super::model::ready::Ready;
+use std::fmt::{Display, Formatter};
 
 /// Görevin kendisini duruma göre tutan enum sabiti.
 #[derive(Debug, PartialEq)]
@@ -8,4 +9,14 @@ pub enum Mission {
     Ready(Ready),
     Doing(Doing),
     Completed(Completed),
+}
+
+impl Display for Mission {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Ready(item) => write!(f, "{}", item.header),
+            Self::Doing(item) => write!(f, "{}", item.header),
+            Self::Completed(item) => write!(f, "{}", item.header),
+        }
+    }
 }

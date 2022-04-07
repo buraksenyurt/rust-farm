7a19c43f4f9d06964b165f9302de74cd0750313f
buraksenyurt <burakselimsenyurt@gmail.com>
2022-04-07 22:41:12 +0300

    Added work item factory function for routing http post request.

--- WebDev/src/planner/src/views/work_item/mod.rs
+++ WebDev/src/planner/src/views/work_item/mod.rs
@@ -1,1 +1,18 @@
+use crate::views::path::Path;
+use actix_web::web;
+
 mod create;
+
+// workitem/create path'ine gelenleri karşılayan factory fonksiyonu
+pub fn work_item_factory(app: &mut web::ServiceConfig) {
+    let base_path = Path {
+        prefix: String::from("workitem"),
+    };
+    // yönlendirme tanımımız.
+    // buna göre path üstünde title ve size bilgileri ile birlikte gelen bir HTTP Post talebi
+    // söz konusu olursa create modülündeki create fonksiyonuna yönlendiriyoruz.
+    app.route(
+        &base_path.define(String::from("/create/{title}/{size}")),
+        web::post().to(create::create),
+    );
+}

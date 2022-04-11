a49dbdbc09fb3c651a29d0a5b6ab9c3a22b1c6f2
buraksenyurt <burakselimsenyurt@gmail.com>
2022-04-11 19:45:36 +0300

    Added the another data structures for scenario.

--- Practices/doc_builder/src/lib.rs
+++ Practices/doc_builder/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter};
+
 // Bir şeyi çizme davranışını tanımlayan yeni bir trait nesnesi eklendi.
 // Tek bir fonksiyonu var uygulandığı nesne ne ise onu referans olarak alıyor
 pub trait Draw {
@@ -34,6 +36,7 @@ pub struct Title {
     pub sub_text: String,
 }
 
+// Title için Draw davranışını modelliyoruz(Sembolik olarak elbette)
 impl Draw for Title {
     fn draw(&self) {
         println!("*****");
@@ -43,12 +46,63 @@ impl Draw for Title {
     }
 }
 
+// Dokümanın alt kısmı için de Bottom isimli bir veri yapısı kurgulayalım
 pub struct Bottom {
     pub summary: String,
 }
 
+// Bottom veri yapısı içinde Draw davranışını yazıyoruz
 impl Draw for Bottom {
     fn draw(&self) {
         println!("{}", self.summary.to_uppercase());
+    }
+}
+
+// Dokümana eklenebilecek ürün bilgilerini LineItems şeklinde bir veri yapısı olarak tutabiliriz.
+// Faturanın orta kısımlarında alt alta kalemlerin yer aldığı, üstünde ürün adı, miktarı ve
+// fiyatının olduğu bir grid düşünün.
+pub struct LineItems {
+    items: Vec<Product>,
+}
+
+impl LineItems {
+    pub fn add(&mut self, p: Product) {
+        self.items.push(p)
+    }
+}
+
+// ve şimdi de LineItems'a Draw davranışını öğretelim
+impl Draw for LineItems {
+    fn draw(&self) {
+        self.items.iter().for_each(|p| {
+            println!("{}", p);
+        })
+    }
+}
+
+// Fatura dokümanında yer alabilecek ürün bilgileri Product struct'ı ile temsil edilebilir.
+pub struct Product {
+    pub id: u32,
+    pub title: String,
+    pub list_price: f32,
+    pub quantity: u16,
+}
+impl Product {
+    pub fn new(id: u32, title: String, list_price: f32, quantity: u16) -> Self {
+        Self {
+            id,
+            title,
+            list_price,
+            quantity,
+        }
+    }
+}
+impl Display for Product {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}|t{}|t{}|t{}",
+            self.id, self.title, self.quantity, self.list_price
+        )
     }
 }

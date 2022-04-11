b9540a51116bc5112e2324c5139302a676908896
buraksenyurt <burakselimsenyurt@gmail.com>
2022-04-11 21:09:23 +0300

    Added some comments to the main function.

--- Practices/doc_builder/src/main.rs
+++ Practices/doc_builder/src/main.rs
@@ -8,19 +8,25 @@ fn main() {
         "Burağın Retro Bilgisayar Dükkanı".to_string(),
         "Fatura. 11.03.2003".to_string(),
     );
-    // Title'ı
+    // Title nesnesini ekliyoruz. Box ile eklediğimize dikkat edelim.
     invoice.add(Box::new(title));
 
+    // Fatura'ya konu olacak iki ürünümüz var. Bunları new metotları ile oluşturuyoruz.
     let mouse = Product::new(1, "Locitek Kablolu Mouse".to_string(), 95.50, 1);
     let keyboard = Product::new(2, "Eyç Pi Q Klavye.".to_string(), 150.00, 1);
+    // Ardından ürünleri line item listesine ilave ediyoruz.
     let mut line_items = LineItems::default();
     line_items.add(mouse);
     line_items.add(keyboard);
 
+    // Ürün bilgilerini tutan nesneyi de faturaya ekliyoruz
     invoice.add(Box::new(line_items));
 
+    // Dokümanın alt kısmını oluşturup ekliyoruz
     let bottom = Bottom::new("İletişim numarası : 23 23 23".to_string());
     invoice.add(Box::new(bottom));
 
+    // Son olarak doküman üstünden print işlevini çağırıyoruz ki bu da esasında
+    // koleksiyona dahil olan tüm nesnelerin Draw fonksiyonunu çağırıyor
     invoice.print();
 }

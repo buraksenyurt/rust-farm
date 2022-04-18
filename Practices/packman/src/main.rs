5885f4c9a344773b64bc91da245b27d55b317ee2
buraksenyurt <burakselimsenyurt@gmail.com>
2022-04-18 22:27:39 +0300

    Completed development of map data structure.

--- Practices/packman/src/main.rs
+++ Practices/packman/src/main.rs
@@ -4,6 +4,7 @@ mod state;
 mod prelude {
     pub const SCHENE_WIDTH: i32 = 80;
     pub const SCHENE_HEIGHT: i32 = 50;
+    pub use bracket_lib::prelude::*;
 }
 fn main() {
     println!("Hello, world!");
--- Practices/packman/src/map.rs
+++ Practices/packman/src/map.rs
@@ -2,7 +2,7 @@ use crate::prelude::*;
 
 const NUMBER_OF_TILES: usize = { SCHENE_WIDTH * SCHENE_HEIGHT } as usize;
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq)]
 pub enum ObjectType {
     Wall,
     Floor,
@@ -19,4 +19,35 @@ impl Map {
             objects: vec![ObjectType::Floor; NUMBER_OF_TILES],
         }
     }
+    pub fn render(&self, ctx: &mut BTerm) {
+        for y in 0..SCHENE_HEIGHT {
+            for x in 0..SCHENE_WIDTH {
+                let index = map_to_index(x, y);
+                match self.objects[index] {
+                    ObjectType::Wall => ctx.set(x, y, WHITE, BLACK, to_cp437('#')),
+                    ObjectType::Floor => ctx.set(x, y, WHITE, BLACK, to_cp437('!')),
+                    ObjectType::Apple => ctx.set(x, y, WHITE, BLACK, to_cp437('.')),
+                    ObjectType::RottenApple => ctx.set(x, y, RED, BLACK, to_cp437(',')),
+                }
+            }
+        }
+    }
+    pub fn is_in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCHENE_WIDTH && point.y >= 0 && point.y < SCHENE_HEIGHT
+    }
+    pub fn want_enter_tile(&self, point: Point) -> bool {
+        self.is_in_bounds(point)
+            && self.objects[map_to_index(point.x, point.y)] == ObjectType::Floor
+    }
+    pub fn try_map_to_index(&self, point: Point) -> Option<usize> {
+        if !self.is_in_bounds(point) {
+            None
+        } else {
+            Some(map_to_index(point.x, point.y))
+        }
+    }
+}
+
+pub fn map_to_index(x: i32, y: i32) -> usize {
+    ((y + SCHENE_WIDTH) + x) as usize
 }

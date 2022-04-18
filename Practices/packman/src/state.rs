66b0c7375877d64c2e303d791b87e4389b75af49
buraksenyurt <burakselimsenyurt@gmail.com>
2022-04-18 22:52:52 +0300

    Completed the coding of state data structure.

--- Practices/packman/src/state.rs
+++ Practices/packman/src/state.rs
@@ -1,1 +1,29 @@
-pub struct State {}
+use crate::prelude::*;
+
+pub struct State {
+    map: Map,
+    packy: Packy,
+}
+
+impl State {
+    pub fn new() -> Self {
+        let mut gen = RandomNumberGenerator::new();
+        let map_builder = MapBuilder::new(&mut gen);
+        Self {
+            map: map_builder.map,
+            packy: Packy::new(map_builder.pack_start),
+        }
+    }
+}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.set_active_console(0);
+        ctx.cls();
+        ctx.set_active_console(1);
+        ctx.cls();
+        self.packy.move_to(ctx, &self.map);
+        self.map.render(ctx);
+        self.packy.render(ctx);
+    }
+}

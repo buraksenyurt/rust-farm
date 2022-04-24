2eea89c9bd8ec32757274a5c504900626d9b1b03
buraksenyurt <burakselimsenyurt@gmail.com>
2022-04-24 19:39:42 +0300

    Redesigned error data structure.

--- handson/imager/src/imagix/error.rs
+++ handson/imager/src/imagix/error.rs
@@ -1,1 +1,24 @@
+use std::{fmt, io};
 
+pub enum ImagixError {
+    FileIO(String),
+    UserInput(String),
+    ImageResizing(String),
+    Format(String),
+}
+
+impl From<io::Error> for ImagixError {
+    fn from(_error: io::Error) -> Self {
+        ImagixError::FileIO("I/O işlemi sırasında hata".to_string())
+    }
+}
+
+impl fmt::Display for ImagixError {
+    fn fmt(&self, out: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(
+            out,
+            "{}",
+            ImagixError::Format("Bir hata oluştu.".to_string())
+        )
+    }
+}

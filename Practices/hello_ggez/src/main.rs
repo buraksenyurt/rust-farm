010d1b0a29372fcc558d553ccfa34954bbe21778
buraksenyurt <burakselimsenyurt@gmail.com>
2022-05-08 23:06:21 +0300

    Added some shapes to window by using state object.

--- Practices/hello_ggez/src/main.rs
+++ Practices/hello_ggez/src/main.rs
@@ -12,9 +12,22 @@ use std::time::Duration;
 */
 
 fn main() {
+    let mut shapes = Vec::new();
+    shapes.push(Shape::Circle(mint::Point2 { x: 200.0, y: 150.0 }, 25.0));
+    shapes.push(Shape::Circle(mint::Point2 { x: 250.0, y: 100.0 }, 25.0));
+    shapes.push(Shape::Rectangle(graphics::Rect::new(
+        290.0, 130.0, 80.0, 10.0,
+    )));
+    shapes.push(Shape::Circle(mint::Point2 { x: 50.0, y: 200.0 }, 25.0));
+    shapes.push(Shape::Rectangle(graphics::Rect::new(
+        10.0, 20.0, 100.0, 10.0,
+    )));
+
     // State nesnesi
     //let state = State::new();
-    let state = State {};
+    let state = State {
+        game_shapes: shapes,
+    };
 
     // Olası konfigurasyon ayarları
     let mut c = Conf::new();
@@ -36,6 +49,12 @@ fn main() {
 // Context enesi ile mouse, klavye, zamanlayıcılar, grafik ve ses gibi donanımlara erişebiliriz.
 struct State {
     //duration: Duration,
+    game_shapes: Vec<Shape>,
+}
+
+enum Shape {
+    Circle(mint::Point2<f32>, f32),
+    Rectangle(graphics::Rect),
 }
 
 // impl State {
@@ -53,23 +72,42 @@ impl EventHandler<GameError> for State {
     }
     fn draw(&mut self, ctx: &mut Context) -> Result<(), GameError> {
         graphics::clear(ctx, graphics::Color::BLACK);
-        let circle = graphics::Mesh::new_circle(
-            ctx,
-            graphics::DrawMode::fill(),
-            mint::Point2 { x: 100.0, y: 20.0 },
-            25.0,
-            0.1,
-            graphics::Color::MAGENTA,
-        )?;
-        graphics::draw(ctx, &circle, graphics::DrawParam::default())?;
-
-        let rectangle = graphics::Mesh::new_rectangle(
-            ctx,
-            graphics::DrawMode::fill(),
-            graphics::Rect::new(100.0, 50.0, 100.0, 20.0),
-            graphics::Color::YELLOW,
-        )?;
-        graphics::draw(ctx, &rectangle, graphics::DrawParam::default())?;
+        for shape in &self.game_shapes {
+            let obj = match shape {
+                &Shape::Rectangle(r) => graphics::Mesh::new_rectangle(
+                    ctx,
+                    graphics::DrawMode::fill(),
+                    r,
+                    graphics::Color::MAGENTA,
+                )?,
+                &Shape::Circle(c, radius) => graphics::Mesh::new_circle(
+                    ctx,
+                    graphics::DrawMode::fill(),
+                    c,
+                    radius,
+                    0.1,
+                    graphics::Color::YELLOW,
+                )?,
+            };
+            graphics::draw(ctx, &obj, graphics::DrawParam::default())?;
+        }
+        // let circle = graphics::Mesh::new_circle(
+        //     ctx,
+        //     graphics::DrawMode::fill(),
+        //     mint::Point2 { x: 100.0, y: 20.0 },
+        //     25.0,
+        //     0.1,
+        //     graphics::Color::MAGENTA,
+        // )?;
+        // graphics::draw(ctx, &circle, graphics::DrawParam::default())?;
+        //
+        // let rectangle = graphics::Mesh::new_rectangle(
+        //     ctx,
+        //     graphics::DrawMode::fill(),
+        //     graphics::Rect::new(100.0, 50.0, 100.0, 20.0),
+        //     graphics::Color::YELLOW,
+        // )?;
+        // graphics::draw(ctx, &rectangle, graphics::DrawParam::default())?;
 
         graphics::present(ctx)?;
         //println!("Duration ={} nanosaniye", self.duration.as_nanos());

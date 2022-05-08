71c4f464e57f9d4bedbf7fd288ef5f02708d862f
buraksenyurt <burakselimsenyurt@gmail.com>
2022-05-08 21:31:01 +0300

    Completed first stage.

--- Practices/hello_ggez/src/main.rs
+++ Practices/hello_ggez/src/main.rs
@@ -1,6 +1,7 @@
-use ggez::event::{Axis, Button, ErrorOrigin, EventHandler, GamepadId, KeyMods, MouseButton};
-use ggez::winit::event::VirtualKeyCode;
-use ggez::{Context, GameError};
+use ggez::conf::Conf;
+use ggez::event::{run, EventHandler};
+use ggez::{timer, Context, ContextBuilder, GameError};
+use std::time::Duration;
 
 /*
    Her oyunun bir döngüsü vardır(Game Loop olarak ifade edilir)
@@ -11,19 +12,43 @@ use ggez::{Context, GameError};
 */
 
 fn main() {
-    let state = State {};
+    // State nesnesi
+    let state = State::new();
+
+    // Olası konfigurasyon ayarları
+    let c = Conf::new();
+    // Her oyunun bir Context ve EventLoop nesnesine ihtiyacı olur
+    let (ctx, event_loop) = ContextBuilder::new("ggez_first_sample", "burak selim")
+        .default_conf(c)
+        .build()
+        .unwrap();
+
+    // Oyunu Context, EventLoop ve State nesneleri ile başlattığımız kısım
+    run(ctx, event_loop, state);
 }
 
 // Oyunun o anki durumuna ait tüm veri ve bilgiler bir State nesnesinde tutulur
 // Oyuncu pozisyonları, skorlar, oyun sahasındaki diğer varlıklar vb
 // Context enesi ile mouse, klavye, zamanlayıcılar, grafik ve ses gibi donanımlara erişebiliriz.
-struct State {}
+struct State {
+    duration: Duration,
+}
+
+impl State {
+    pub fn new() -> Self {
+        Self {
+            duration: Duration::new(0, 0),
+        }
+    }
+}
 
 impl EventHandler<GameError> for State {
-    fn update(&mut self, _ctx: &mut Context) -> Result<(), GameError> {
+    fn update(&mut self, ctx: &mut Context) -> Result<(), GameError> {
+        self.duration = timer::delta(ctx);
         Ok(())
     }
     fn draw(&mut self, _ctx: &mut Context) -> Result<(), GameError> {
+        println!("Duration ={}nano saniye", self.duration.as_nanos());
         Ok(())
     }
 }

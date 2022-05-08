b6a908e994d231da88ebd5a17945d9378d485a38
buraksenyurt <burakselimsenyurt@gmail.com>
2022-05-08 22:58:06 +0300

    Added some shapes to window.

--- Practices/hello_ggez/src/main.rs
+++ Practices/hello_ggez/src/main.rs
@@ -1,6 +1,6 @@
-use ggez::conf::Conf;
+use ggez::conf::{Conf, WindowMode};
 use ggez::event::{run, EventHandler};
-use ggez::{timer, Context, ContextBuilder, GameError};
+use ggez::{graphics, mint, timer, Context, ContextBuilder, GameError};
 use std::time::Duration;
 
 /*
@@ -13,16 +13,20 @@ use std::time::Duration;
 
 fn main() {
     // State nesnesi
-    let state = State::new();
+    //let state = State::new();
+    let state = State {};
 
     // Olası konfigurasyon ayarları
-    let c = Conf::new();
+    let mut c = Conf::new();
+    c.window_setup.title = "First Stage Game".to_string();
+    c.window_mode.width = 400.0;
+    c.window_mode.height = 400.0;
+
     // Her oyunun bir Context ve EventLoop nesnesine ihtiyacı olur
     let (ctx, event_loop) = ContextBuilder::new("ggez_first_sample", "burak selim")
         .default_conf(c)
         .build()
         .unwrap();
-
     // Oyunu Context, EventLoop ve State nesneleri ile başlattığımız kısım
     run(ctx, event_loop, state);
 }
@@ -31,24 +35,44 @@ fn main() {
 // Oyuncu pozisyonları, skorlar, oyun sahasındaki diğer varlıklar vb
 // Context enesi ile mouse, klavye, zamanlayıcılar, grafik ve ses gibi donanımlara erişebiliriz.
 struct State {
-    duration: Duration,
+    //duration: Duration,
 }
 
-impl State {
-    pub fn new() -> Self {
-        Self {
-            duration: Duration::new(0, 0),
-        }
-    }
-}
+// impl State {
+//     pub fn new() -> Self {
+//         Self {
+//             duration: Duration::new(0, 0),
+//         }
+//     }
+// }
 
 impl EventHandler<GameError> for State {
-    fn update(&mut self, ctx: &mut Context) -> Result<(), GameError> {
-        self.duration = timer::delta(ctx);
+    fn update(&mut self, _ctx: &mut Context) -> Result<(), GameError> {
+        //self.duration = timer::delta(ctx);
         Ok(())
     }
-    fn draw(&mut self, _ctx: &mut Context) -> Result<(), GameError> {
-        println!("Duration ={}nano saniye", self.duration.as_nanos());
+    fn draw(&mut self, ctx: &mut Context) -> Result<(), GameError> {
+        graphics::clear(ctx, graphics::Color::BLACK);
+        let circle = graphics::Mesh::new_circle(
+            ctx,
+            graphics::DrawMode::fill(),
+            mint::Point2 { x: 100.0, y: 20.0 },
+            25.0,
+            0.1,
+            graphics::Color::MAGENTA,
+        )?;
+        graphics::draw(ctx, &circle, graphics::DrawParam::default())?;
+
+        let rectangle = graphics::Mesh::new_rectangle(
+            ctx,
+            graphics::DrawMode::fill(),
+            graphics::Rect::new(100.0, 50.0, 100.0, 20.0),
+            graphics::Color::YELLOW,
+        )?;
+        graphics::draw(ctx, &rectangle, graphics::DrawParam::default())?;
+
+        graphics::present(ctx)?;
+        //println!("Duration ={} nanosaniye", self.duration.as_nanos());
         Ok(())
     }
 }

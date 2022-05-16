12d4dcb6d90f317b048aedaf7a88b545ddfd70c8
buraksenyurt <burakselimsenyurt@gmail.com>
2022-05-16 21:32:09 +0300

    Added {:p} usage.

--- Practices/somethings/src/main.rs
+++ Practices/somethings/src/main.rs
@@ -4,6 +4,22 @@ fn main() {
     println!("{:?}", x);
     println!("{:#?}", x);
     println!("{} {}", x.r#in, x.r#for);
+
+    // i32 scalar tipi için atamalar ve bellek adresleri
+    let number1 = 32;
+    let number2 = &number1;
+    let number3 = &number2;
+
+    println!("number1 value {} and address {:p}", number1, &number1);
+    println!("number2 value {} and address {:p}", number2, number2);
+    println!("number3 value {} and address {:p}", number3, number3);
+
+    let number4 = &&23;
+    println!("number4 address {:p}", &&number4);
+    println!("number4 address {:p}", number4);
+    println!("number4 value {}", number4);
+
+    println!("x address {:p}",&x);
 }
 
 // Raw Identifier Örneği

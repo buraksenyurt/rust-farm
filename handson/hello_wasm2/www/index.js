55af403c644d62395c6464049c92b973c79183cb
buraksenyurt <burakselimsenyurt@gmail.com>
2022-05-20 17:21:50 +0300

    Added sample JS object which will use by WASM.

--- handson/hello_wasm2/www/index.js
+++ handson/hello_wasm2/www/index.js
@@ -1,12 +1,20 @@
 async function run() {
+    // Aşağıdaki nesne WASM tarafında kullanılabilir
+    const logObject = {
+        console: {
+            log: ()=> {
+                console.log("Örnek log");
+            }
+        }
+    }
 
-  const response = await fetch("calc.wasm");
-  const buffer = await response.arrayBuffer();
-  const wasm = await WebAssembly.instantiate(buffer);
+    const response = await fetch("calc.wasm");
+    const buffer = await response.arrayBuffer();
+    const wasm = await WebAssembly.instantiate(buffer,logObject);
 
-  const calcFunc = wasm.instance.exports.calc;
-  const result = calcFunc(2);
-  console.log(result);
+    const calcFunc = wasm.instance.exports.calc;
+    const result = calcFunc(2);
+    console.log(result);
 }
 
 run();

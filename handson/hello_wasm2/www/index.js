d760366581ecfa06deef1b73822a5989f7bc77f8
buraksenyurt <burakselimsenyurt@gmail.com>
2022-05-28 13:33:49 +0300

    Added memory page into WASM.

--- handson/hello_wasm2/www/index.js
+++ handson/hello_wasm2/www/index.js
@@ -15,6 +15,14 @@ async function run() {
     const calcFunc = wasm.instance.exports.calc;
     const result = calcFunc(2);
     console.log(result);
+
+    // WASM İçerisinde tanımlanmış olan bellek sayfasına erişim sağlanıyor
+    const memoryFromWasm = wasm.instance.exports.mem;
+    // WASM İçinden gelen bellek sayfasının ilk 32 byte'ını okumak için bir array tanımlanır
+    const message_buffer = new Uint8Array(memoryFromWasm.buffer,0,32);
+    // sonrasında TextDecoder'tan yararlanılarak byte dizisi decode edilir(okunur hale getirilir)
+    const message = new TextDecoder().decode(message_buffer);
+    console.log(message);
 }
 
 run();

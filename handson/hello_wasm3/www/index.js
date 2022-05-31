dce55faf374104f3d26b543f3684bf47c89d5d27
buraksenyurt <burakselimsenyurt@gmail.com>
2022-05-31 18:40:55 +0300

    Added alternative usage for calling wasm function from js.

--- handson/hello_wasm3/www/index.js
+++ handson/hello_wasm3/www/index.js
@@ -4,3 +4,12 @@ init().then(_ => {
     var info = print_temperature("istanbul");
     console.log(info);
 })
+
+// Alttaki kullanım yerine genellikle üstteki kullanım da tercih edilebilir.
+//async function start(){
+//    const wasm = await init();
+//    var info = print_temperature("istanbul");
+//    console.log(info);
+//}
+//
+//start();

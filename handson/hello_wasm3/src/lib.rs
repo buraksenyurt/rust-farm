074b8d5ec8abed83137018c7955b82e824301d08
buraksenyurt <burakselimsenyurt@gmail.com>
2022-05-31 16:43:43 +0300

    Added new WASM sample project which is use Rust.

--- handson/hello_wasm3/src/lib.rs
+++ handson/hello_wasm3/src/lib.rs
@@ -1,6 +1,9 @@
+use rand::Rng;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn print_temperature(city: &str) {
-    println!("{} şehrindeki sıcaklık 32 derecedir", city);
+pub fn print_temperature(city: &str) -> String {
+    let mut rng = rand::thread_rng();
+    let temperature = rng.gen_range(-10..50);
+    format!("{} için sıcaklık {} derece", city, temperature)
 }
--- handson/hello_wasm3/www/index.js
+++ handson/hello_wasm3/www/index.js
@@ -1,40 +1,6 @@
-async function run() {
+import init, {print_temperature} from "hello_wasm3";
 
-    // 64 Kb'lık (one page) bir bellek sayfası tanımlanır
-    // bunu yine logObject içerisinde tanımlayıp WASM tarafında ele alınmasını sağlayabiliriz.
-    const js_memory = new WebAssembly.Memory({initial:1});
-
-    // Aşağıdaki nesne WASM tarafında kullanılabilir
-    const jsObject = {
-        js: {
-            mem : js_memory
-        },
-        console: {
-            log: (param)=> {
-                console.log(param+" için alan hesaplanacak.");
-            }
-        }
-    }
-
-    const response = await fetch("calc.wasm");
-    const buffer = await response.arrayBuffer();
-    const wasm = await WebAssembly.instantiate(buffer,jsObject);
-
-    const calcFunc = wasm.instance.exports.calc;
-    const result = calcFunc(2);
-    console.log(result);
-
-//    // WASM İçerisinde tanımlanmış olan bellek sayfasına erişim sağlanıyor
-//    const memoryFromWasm = wasm.instance.exports.mem;
-//    // WASM İçinden gelen bellek sayfasının ilk 32 byte'ını okumak için bir array tanımlanır
-//    const message_buffer = new Uint8Array(memoryFromWasm.buffer,0,32);
-//    // sonrasında TextDecoder'tan yararlanılarak byte dizisi decode edilir(okunur hale getirilir)
-//    const message = new TextDecoder().decode(message_buffer);
-
-    // JS tarafında tanımlı bellek bölgesini yukarıda tanımlamıştık
-    const message_buffer = new Uint8Array(js_memory.buffer,0,32);
-    const message = new TextDecoder().decode(message_buffer);
-    console.log(message);
-}
-
-run();
+init().then(_ => {
+    var info = print_temperature("istanbul");
+    console.log(info);
+})

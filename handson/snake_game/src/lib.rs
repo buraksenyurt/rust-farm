df5d6e6e0494bc4d5413fa3a394cf2d996841daa
buraksenyurt <burakselimsenyurt@gmail.com>
2022-06-05 13:29:59 +0300

    Added snake data structure to the game.

--- handson/snake_game/src/lib.rs
+++ handson/snake_game/src/lib.rs
@@ -6,16 +6,38 @@ static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
 #[wasm_bindgen]
 pub struct World {
-    pub width: usize,
+    width: usize,
+    snake: Snake,
 }
 
 #[wasm_bindgen]
 impl World {
     pub fn new() -> Self {
-        Self { width: 8 }
+        Self {
+            width: 8,
+            snake: Snake::new(10),
+        }
     }
 
     pub fn get_width(&self) -> usize {
         self.width
+    }
+
+    pub fn snake_head(&self) -> usize {
+        self.snake.body[0].0
+    }
+}
+
+struct SnakeCell(usize);
+
+struct Snake {
+    body: Vec<SnakeCell>,
+}
+
+impl Snake {
+    fn new(start_index: usize) -> Self {
+        Self {
+            body: vec![SnakeCell(start_index)],
+        }
     }
 }

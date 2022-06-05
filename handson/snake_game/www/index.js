98a537a3c8fcbd43b4dbc616553d6218ebb64d27
buraksenyurt <burakselimsenyurt@gmail.com>
2022-06-05 11:57:59 +0300

    Added canvas element to UI.

--- handson/snake_game/www/index.js
+++ handson/snake_game/www/index.js
@@ -2,5 +2,9 @@ import init, { World } from "snake_game";
 
 init().then(_ => {
     const world = World.new();
-    console.log("Oyun nesnesi oluşturuldu. Uzunluk " + world.width + " birim.");
+    console.log("Oyun nesnesi oluşturuldu. Uzunluk " + world.get_width() + " birim.");
+
+    // canvas elementini alıp 2 boyutlu context nesnesine ulaşıyoruz.
+    const canvas = document.getElementById("game-canvas");
+    const canvas_context=canvas.getContext("2d");
 })

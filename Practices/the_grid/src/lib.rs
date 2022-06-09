079da890ce97b8f15dbce0fa036ca08fedf423ff
buraksenyurt <burakselimsenyurt@gmail.com>
2022-06-09 22:05:09 +0300

    Added random number generator for grid rows and columns.

--- Practices/the_grid/src/lib.rs
+++ Practices/the_grid/src/lib.rs
@@ -1,4 +1,4 @@
-use std::fmt::{Display, Formatter};
+use rand::Rng;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -7,7 +7,7 @@ pub struct GameGrid {
 }
 
 #[wasm_bindgen]
-#[derive(Clone,Copy)]
+#[derive(Clone, Copy)]
 pub struct GridSize {
     pub rows: usize,
     pub columns: usize,
@@ -22,9 +22,20 @@ impl GridSize {
 
 #[wasm_bindgen]
 impl GameGrid {
-    pub fn new(rows: usize, columns: usize) -> Self {
+    pub fn new() -> Self {
+        let mut rng = rand::thread_rng();
+        let row_count = rng.gen_range(5..16);
+        let column_count = rng.gen_range(5..16);
         Self {
-            size: GridSize::new(rows, columns),
+            size: GridSize::new(row_count, column_count),
+        }
+    }
+
+    pub fn get_max_len(&self) -> usize {
+        if self.size.rows > self.size.columns {
+            self.size.rows
+        } else {
+            self.size.columns
         }
     }
 }
--- Practices/the_grid/www/index.js
+++ Practices/the_grid/www/index.js
@@ -2,9 +2,31 @@ import init, {GameGrid,GridSize} from "the_grid";
 
 init().then(_=>{
     const CELL_SIZE = 32;
-    const ROW_SIZE = 12;
-    const COLUMN_SIZE = 8;
-    const grid = GameGrid.new(ROW_SIZE,COLUMN_SIZE);
-
+    const grid = GameGrid.new();
     console.log(grid.size.rows + ' X ' + grid.size.columns);
+
+    const max_width = grid.get_max_len();
+    const canvas = document.getElementById("game-canvas");
+    canvas.height = grid.size.rows * CELL_SIZE;
+    canvas.width = grid.size.columns * CELL_SIZE;
+
+    const canvas_context=canvas.getContext("2d");
+
+    function drawGrid(){
+        canvas_context.beginPath();
+
+        for (let x = 0; x < max_width+ 1; x++){
+            canvas_context.moveTo(CELL_SIZE * x, 0);
+            canvas_context.lineTo(CELL_SIZE * x, max_width * CELL_SIZE);
+        }
+
+        for (let y = 0; y < max_width + 1; y++){
+            canvas_context.moveTo(0, CELL_SIZE * y);
+            canvas_context.lineTo(max_width * CELL_SIZE,CELL_SIZE * y);
+        }
+
+        canvas_context.stroke();
+    }
+
+    drawGrid();
 })

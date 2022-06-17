ac2c518fe6db85262261c4a2eed3f461f0c6af42
buraksenyurt <burakselimsenyurt@gmail.com>
2022-06-17 22:40:38 +0300

    Changed web forms and added click listener.

--- Practices/booksworm/www/index.js
+++ Practices/booksworm/www/index.js
@@ -2,4 +2,10 @@ import init,{Book} from "booksworm";
 
 init().then(_ =>{
     console.log("It's working...");
+
+    const submitButton = document.getElementById("buttonAdd");
+
+    submitButton.addEventListener("click",_=>{
+        console.log("Ekle düğmesine basıldı");
+    });
 });

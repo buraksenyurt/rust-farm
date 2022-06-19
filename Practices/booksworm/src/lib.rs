33c490ea6103e55177543885b7ee28c336c0eeb6
buraksenyurt <burakselimsenyurt@gmail.com>
2022-06-19 12:50:22 +0300

    Added book insert function.

--- Practices/booksworm/src/lib.rs
+++ Practices/booksworm/src/lib.rs
@@ -1,15 +1,40 @@
+use std::fmt::{Display, Formatter};
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-pub struct Book{
-
+pub struct Book {
+    pub title: String,
+    pub authors: String,
+    pub is_read: bool,
 }
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+impl Book {
+    pub fn new(title: String, authors: String, is_read: bool) -> Self {
+        Self {
+            title,
+            authors,
+            is_read,
+        }
     }
 }
+
+impl Display for Book {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} ({})", self.title, self.authors)
+    }
+}
+
+#[wasm_bindgen]
+pub fn add_book(title: String, authors: String, is_read: bool) -> bool {
+    let book = Book::new(title, authors, is_read);
+    println!("{}", book.to_string());
+    true
+}
+
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         let result = 2 + 2;
+//         assert_eq!(result, 4);
+//     }
+// }
--- Practices/booksworm/www/index.js
+++ Practices/booksworm/www/index.js
@@ -1,11 +1,20 @@
-import init,{Book} from "booksworm";
+import init,{Book,add_book} from "booksworm";
 
 init().then(_ =>{
-    console.log("It's working...");
-
     const submitButton = document.getElementById("buttonAdd");
 
     submitButton.addEventListener("click",_=>{
         console.log("Ekle düğmesine basıldı");
+
+        var title=document.getElementById("inputTitle").value;
+        var authors=document.getElementById("inputAuthors").value;
+        var isRead=document.getElementById("checkIsRead").checked;
+
+        console.log(title);
+        console.log(authors);
+        console.log(isRead);
+
+        var result=add_book(title,authors,isRead);
+        console.log(result);
     });
 });

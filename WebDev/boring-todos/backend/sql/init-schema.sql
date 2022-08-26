603a7e5f91ec26a60fe90cb769942bb78204a297
buraksenyurt <burakselimsenyurt@gmail.com>
2022-08-26 12:50:48 +0300

    Added database queries for initialization.

---
+++ WebDev/boring-todos/backend/sql/init-schema.sql
@@ -0,0 +1,14 @@
+CREATE TYPE task_state AS ENUM (
+    'ready',
+    'in-progress',
+    'completed'
+);
+
+CREATE TABLE task(
+    id bigserial,
+    user_id bigint NOT NULL,
+    create_time timestamp with time zone DEFAULT now(),
+    title text NOT NULL,
+    state task_state NOT NULL DEFAULT 'ready'
+);
+ALTER SEQUENCE task_id_sequence RESTART WITH 1000;
---
+++ WebDev/boring-todos/backend/sql/seed-data.sql
@@ -0,0 +1,2 @@
+INSERT INTO task (id,user_id,title) VALUES (1,10066,'Odayı temizle');
+INSERT INTO task (id,user_id,title,"state") VALUES (1,10066,'Matematik ödev ikiyi tamamla','completed');

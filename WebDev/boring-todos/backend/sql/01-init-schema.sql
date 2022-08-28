48401111981b971cdc86201e2020c3432b12e7b2
buraksenyurt <burakselimsenyurt@gmail.com>
2022-08-28 14:08:09 +0300

    Some modifications for state type.

--- WebDev/boring-todos/backend/sql/01-init-schema.sql
+++ WebDev/boring-todos/backend/sql/01-init-schema.sql
@@ -1,13 +1,13 @@
 CREATE TYPE task_state AS ENUM (
-    'ready',
-    'in-progress',
-    'completed'
+    'Ready',
+    'InProgress',
+    'Completed'
 );
 CREATE TABLE task(
     id bigserial,
     user_id bigint NOT NULL,
     create_time timestamp with time zone DEFAULT now(),
     title text NOT NULL,
-    state task_state NOT NULL DEFAULT 'ready'
+    state task_state NOT NULL DEFAULT 'Ready'
 );
 ALTER SEQUENCE task_id_sequence RESTART WITH 1000;

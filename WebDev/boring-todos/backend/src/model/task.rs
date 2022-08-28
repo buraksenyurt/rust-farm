ab315de72037fb4d7a46971fe9ac47a83359ae42
buraksenyurt <burakselimsenyurt@gmail.com>
2022-08-28 14:00:19 +0300

    Added the task create functionality.

--- WebDev/boring-todos/backend/src/model/task.rs
+++ WebDev/boring-todos/backend/src/model/task.rs
@@ -8,6 +8,12 @@ pub struct Task {
     pub title: String,
 }
 
+#[derive(Clone)]
+pub struct TaskDao {
+    pub user_id: Option<i64>,
+    pub title: Option<String>,
+}
+
 pub struct TaskMac; // Mac = Model Access Controller
 
 impl TaskMac {
@@ -18,12 +24,23 @@ impl TaskMac {
 
         Ok(task_list)
     }
+
+    pub async fn create(db: &Db, payload: TaskDao) -> Result<Task, Error> {
+        let sql = "INSERT INTO task (user_id,title) VALUES ($1,$2) returning id,user_id,title";
+        let query = sqlx::query_as::<_, Task>(&sql)
+            .bind(payload.user_id)
+            .bind(payload.title);
+
+        let created_task = query.fetch_one(db).await?;
+
+        Ok(created_task)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::model::database::init;
-    use crate::model::task::TaskMac;
+    use crate::model::task::{TaskDao, TaskMac};
 
     #[tokio::test]
     async fn should_get_all_returns_more_than_one_task() -> Result<(), Box<dyn std::error::Error>> {
@@ -31,6 +48,22 @@ mod tests {
         let result = TaskMac::get_all(&db).await?;
         assert!(result.len() > 0, "Görevler listesi");
 
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn should_create_task_works_with_some_datas() -> Result<(), Box<dyn std::error::Error>> {
+        let db = init().await?;
+
+        let candidate_task = TaskDao {
+            user_id: Some(4835),
+            title: Some(String::from("Rust programlama için bir saat çalış")),
+        };
+
+        let created_task = TaskMac::create(&db, candidate_task.clone()).await?;
+        assert!(created_task.id >= 1);
+        assert_eq!(candidate_task.user_id.unwrap(), created_task.user_id);
+        assert_eq!(candidate_task.title.unwrap(), created_task.title);
         Ok(())
     }
 }

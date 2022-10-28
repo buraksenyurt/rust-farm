caedeebc2e35c46162575e1118092d2a080cc60f
buraksenyurt <burakselimsenyurt@gmail.com>
2022-10-28 23:01:55 +0300

    Added json support and first post function.

--- handson/song-api/src/main.rs
+++ handson/song-api/src/main.rs
@@ -1,10 +1,23 @@
-//noinspection RsMainFunctionNotFound
+/*
+   Bir istek parça listesi için Restful API hizmeti vermek istediğimiz düşünelim.
+   İstek listesini tutacak veri yapısının bir kuyruk olarak ele alınması gerekir.
+   Veri içeriği tüm istemciler için paylaşımlıdır.
+   Eş zamanlı veri erişimini kontrol altına almak için Mutex yapısından yararlanılabilir.
+
+   Tutorial kaynağımız : https://imajindevon.hashnode.dev/rust-rocket-song-request-api
+
+   Post örneği
+
+   curl -X POST http://127.0.0.1:8000/add -H Content-Type: "application/json" -d
+   '{"title": "Domates Biber Patlıcan","artist": "Barış Manço","album": "Mançoloji II"}'
+*/
 mod song;
 
 #[macro_use]
 extern crate rocket;
 
 use crate::song::Song;
+use rocket::serde::json::Json;
 use rocket::{Build, Rocket};
 use std::collections::LinkedList;
 use std::sync::{Mutex, MutexGuard};
@@ -15,9 +28,20 @@ static SONG_QUEUE: Mutex<LinkedList<Song>> = Mutex::new(LinkedList::new());
 
 #[launch]
 fn rocket() -> Rocket<Build> {
-    Rocket::build().mount("/", routes![])
+    Rocket::build().mount("/", routes![add_song])
 }
 
 fn get_queue<'a>() -> MutexGuard<'a, LinkedList<Song>> {
     SONG_QUEUE.lock().expect("Mutex ile bir sorun yaşanıyor")
+}
+
+#[post("/add", data = "<s>")]
+fn add_song(s: Json<Song>) -> String {
+    let mut locked_object = get_queue();
+    locked_object.push_back(s.0.clone());
+    format!(
+        "[{}] isimli şarkı {}. sıraya eklendi.",
+        s.title,
+        locked_object.len()
+    )
 }
--- handson/song-api/src/song.rs
+++ handson/song-api/src/song.rs
@@ -1,5 +1,18 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Serialize, Deserialize)]
 pub struct Song {
-    title: String,
-    album: String,
-    artist: String,
+    pub title: String,
+    pub album: String,
+    pub artist: String,
+}
+
+impl Song {
+    pub fn new(title: String, album: String, artist: String) -> Self {
+        Self {
+            title,
+            album,
+            artist,
+        }
+    }
 }

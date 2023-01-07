e9f24f7ccfa663c1a4495bbb336f7b5d5b8fc0d2
buraksenyurt <burakselimsenyurt@gmail.com>
2023-01-07 18:39:51 +0300

    Integrated the read from file ability.

--- handson/curlc/src/tests/mod.rs
+++ handson/curlc/src/tests/mod.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod tests {
     use crate::io::parser::parse_line;
+    use crate::io::reader::read_file;
     use crate::transport::http::Method::{Get, Post};
     use crate::transport::http::{HttpParseError, HttpRequest};
 
@@ -31,5 +32,28 @@ mod tests {
         let response = parse_line("houston we have a problem");
         let expected = Err(HttpParseError::UnrecognizedFormat);
         assert_eq!(response, expected);
+    }
+
+    #[test]
+    fn should_reader_works() {
+        let request = read_file("initial.req");
+        let expected = vec![
+            Ok(HttpRequest {
+                method: Get,
+                url: "google.com".to_string(),
+            }),
+            Ok(HttpRequest {
+                method: Post {
+                    body: "{\"title\" : \"Interstellar\"}".to_string(),
+                },
+                url: "movies/api".to_string(),
+            }),
+            Err(HttpParseError::UnrecognizedFormat),
+            Ok(HttpRequest {
+                method: Get,
+                url: "buraksenyurt.com".to_string(),
+            }),
+        ];
+        assert_eq!(request, expected);
     }
 }

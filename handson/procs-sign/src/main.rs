35523b69f500991d2b2f9824eb0a5e7b51389eba
buraksenyurt <burakselimsenyurt@gmail.com>
2023-01-15 14:59:56 +0300

    Handling the I/O of child processes.

--- handson/procs-sign/src/main.rs
+++ handson/procs-sign/src/main.rs
@@ -1,5 +1,10 @@
+/*
+   Practical System Programming for Rust Developers kitabından çalıştığım
+   Working with Process and Signals bölümüne ait kodlar ve açıklamaları.
+*/
+use std::io::{Read, Write};
 use std::process;
-use std::process::{Command};
+use std::process::{Command, Stdio};
 
 fn main() {
     // // İşletim sisteminden bir programı process olarak açmak için
@@ -42,8 +47,62 @@ fn main() {
     if cat_status.success() {
         println!("İşlem başarılı");
     } else {
-        println!("İşlem başarısız");
+        println!("cat there-is-no-file.txt komutu başarısız");
     }
+
+    // Bir Parent Process ile başlattığı Child Process arasında iletişim hattı(pipe) açıp
+    // process çıktılarını ele alabiliriz.
+    // ps komutu ile sistemde çalışan process'lere ait bilgileri görüntüleyebiliriz.
+    // Aşağıdaki kod parçasında ps için bir child process açılır ve çıktısı açılan pipe aracılığı
+    // ile process_output isimli bir String değişkene alınır.
+    let process = match Command::new("ps").arg("a").stdout(Stdio::piped()).spawn() {
+        Ok(process) => process,
+        Err(e) => panic!("ps komutu işletilemedi. {}", e),
+    };
+    let mut process_output = String::new();
+    match process.stdout.unwrap().read_to_string(&mut process_output) {
+        Ok(_) => println!("Process Listesi\n{}", &process_output),
+        Err(e) => println!("ps komutunun çıktısı okunamadı. {}", e),
+    };
+
+    // Aşağıdaki kod parçasında ise parent process'ten child process'e veri gönderimi
+    // ele alınmaktadır. Bunu deneyimlemek için bir metin katarını tersine döndüren rev komutu
+    // kullanılmakta.
+
+    // önce process handler oluşturulur.
+    // Bu yapılırken child process ile piped türünden hem girdi hem de çıktı için
+    // çift yönlü hat tesis edilir.
+    // rev komutuna dikkat edileceği üzere bir argüman henüz gönderilmedi.
+    let rev_process = match Command::new("rev")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+    {
+        Ok(prc) => prc,
+        Err(e) => panic!("rev komutu oluşturulurken hata. {}", e),
+    };
+
+    // process handler'in girdi hattı kullanılarak
+    // rev komutunun çalıştırması istenen bilgi gönderilir
+    match &rev_process
+        .stdin
+        .unwrap()
+        .write_all("Hello from New England".as_bytes())
+    {
+        Ok(_) => println!("rev komutuna bilgi gönderildi"),
+        Err(e) => println!("rev komutuna arguman gönderilemedi {}", e),
+    };
+
+    // alt process'in işlettiği komutun çıktısını almak içinde stdout kullanılır.
+    let mut child_output = String::new();
+    match &rev_process
+        .stdout
+        .unwrap()
+        .read_to_string(&mut child_output)
+    {
+        Ok(_) => println!("\t{}", child_output),
+        Err(e) => println!("rev işlem sonucu alınamadı.{}", e),
+    };
 
     // // Bir process'si sonlandırmak için iki yöntem var.
     // // abort ve exit.

07763d24071acd8eb5d5235924b6a4dd7f428e05
buraksenyurt <burakselimsenyurt@gmail.com>
2023-01-20 18:10:10 +0300

    Installed the logging infrastructure.

--- handson/jwt-api/src/main.rs
+++ handson/jwt-api/src/main.rs
@@ -1,14 +1,19 @@
 mod model;
 mod network;
+
+use log::info;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
+    // loglama ayarlarını içeren dosyayı yüklüyoruz
+    log4rs::init_file("log_config.yml", Default::default()).expect("Config dosyası bulunamadı");
+    info!("Sunucu çalıştırılıyor...");
     // Başlangıç için kullanılacak bir wellcome sayfası
     let root = warp::path::end().map(|| "Wellcome page");
     // CORS ayarlaması. Herhangi bir kaynaktan gelinebilir
     let routes = root.with(warp::cors().allow_any_origin());
-    println!("Sunucu dinlemede. 127.0.0.1:5555");
+    info!("Sunucu dinlemede. http:://127.0.0.1:5555");
     // 127.0.0.1:5555 porttan sunucu açılır
     warp::serve(routes).run(([127, 0, 0, 1], 5555)).await;
 }

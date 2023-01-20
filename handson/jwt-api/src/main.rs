ab30336c046c2b9b115873fb84beaae3fa1f874a
buraksenyurt <burakselimsenyurt@gmail.com>
2023-01-20 18:02:42 +0300

    Added basic http server capabilities.

--- handson/jwt-api/src/main.rs
+++ handson/jwt-api/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+mod model;
+mod network;
+use warp::Filter;
+
+#[tokio::main]
+async fn main() {
+    // Başlangıç için kullanılacak bir wellcome sayfası
+    let root = warp::path::end().map(|| "Wellcome page");
+    // CORS ayarlaması. Herhangi bir kaynaktan gelinebilir
+    let routes = root.with(warp::cors().allow_any_origin());
+    println!("Sunucu dinlemede. 127.0.0.1:5555");
+    // 127.0.0.1:5555 porttan sunucu açılır
+    warp::serve(routes).run(([127, 0, 0, 1], 5555)).await;
 }

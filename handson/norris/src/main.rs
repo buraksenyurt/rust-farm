6a6b8fbdd5f77373d7126c5e974b7bd2c2a869a5
buraksenyurt <burakselimsenyurt@gmail.com>
2023-03-20 16:24:52 +0300

    Added handler which get random norris fact.

--- handson/norris/src/main.rs
+++ handson/norris/src/main.rs
@@ -1,11 +1,39 @@
-use std::fs::{read, File};
+use axum::routing::get;
+use axum::{Json, Router};
+use rand::Rng;
+use std::fs::File;
 use std::io;
 use std::io::BufRead;
+use std::net::SocketAddr;
 use std::path::Path;
 
-fn main() {}
+#[tokio::main]
+async fn main() {
+    let route = Router::new().route("/", get(default_handler));
+    let address = SocketAddr::from(([127, 0, 0, 1], 6001));
+    println!(
+        "Sunucu başlatılıyor: {}.\nÇek bir Chuck Norris sözü.",
+        address
+    );
+
+    axum::Server::bind(&address)
+        .serve(route.into_make_service())
+        .await
+        .expect("Failed to start server");
+}
+
+async fn default_handler() -> Json<Fact> {
+    let facts = load_facts().unwrap();
+    let mut rng = rand::thread_rng();
+    let index: usize = rng.gen_range(0..facts.len()) as usize;
+    if let Some(f) = facts.get(index) {
+        return Json(f.clone());
+    }
+    Json(Fact::new(0, "May be another time".to_string()))
+}
 
 fn load_facts() -> Option<Vec<Fact>> {
+    println!("Norris facts are loading");
     if let Ok(lines) = read_lines("./norris_facts.dat") {
         let mut facts: Vec<Fact> = Vec::new();
         for (i, line) in lines.enumerate() {
@@ -26,6 +54,7 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
+#[derive(serde::Serialize, Clone)]
 struct Fact {
     id: u32,
     content: String,
@@ -38,13 +67,13 @@ impl Fact {
 }
 
 #[cfg(test)]
-mod test{
+mod test {
     use crate::load_facts;
 
     #[test]
-    fn read_file_lines_test(){
-        let facts=load_facts();
+    fn read_file_lines_test() {
+        let facts = load_facts();
         assert!(facts.is_some());
-        assert!(facts.unwrap().len()>0);
+        assert!(facts.unwrap().len() > 0);
     }
 }

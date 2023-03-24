bdae98553b2f7bc6f7870798e96a2bba06778b74
buraksenyurt <burakselimsenyurt@gmail.com>
2023-03-25 01:04:17 +0300

    Added support to suggest random book by category.

--- handson/bookshop-grpc-service/src/bookshop_service.rs
+++ handson/bookshop-grpc-service/src/bookshop_service.rs
@@ -1,5 +1,7 @@
 use crate::bookshop::bookshop_server::Bookshop;
 use crate::bookshop::{GetSuggestionRequest, GetSuggestionResponse};
+use crate::store::{load_books, Book};
+use rand::{thread_rng, Rng};
 use tonic::{Request, Response, Status};
 
 #[derive(Default)]
@@ -13,14 +15,33 @@ impl Bookshop for BookShopService {
     ) -> Result<Response<GetSuggestionResponse>, Status> {
         println!("Gelen talep {:?}", request.remote_addr());
 
-        let suggestion = GetSuggestionResponse {
-            book_id: 1,
-            title: "Sistem".to_string(),
-            author: "James Ball".to_string(),
-            category: request.into_inner().category,
-            unit_price: 34.50,
-        };
+        if let Some(books) = load_books() {
+            let founded: Vec<Book> = books
+                .into_iter()
+                .filter(|b| b.category == request.get_ref().category)
+                .collect();
 
-        Ok(Response::new(suggestion))
+            if !founded.is_empty() {
+                let mut rng = thread_rng();
+                let idx = rng.gen_range(0..founded.len());
+                let b: Book = founded[idx].clone();
+                let suggestion = GetSuggestionResponse {
+                    book_id: b.book_id,
+                    title: b.title,
+                    author: b.authors,
+                    category: b.category,
+                    unit_price: b.unit_price,
+                };
+                return Ok(Response::new(suggestion));
+            }
+        }
+
+        Ok(Response::new(GetSuggestionResponse {
+            book_id: 0,
+            title: "Not found".to_string(),
+            author: "Not found".to_string(),
+            category: "Not found".to_string(),
+            unit_price: 0.0,
+        }))
     }
 }
--- handson/bookshop-grpc-service/src/store.rs
+++ handson/bookshop-grpc-service/src/store.rs
@@ -2,12 +2,13 @@ use serde::Deserialize;
 use std::fs::File;
 use std::io::BufReader;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Clone)]
 pub struct Book {
     pub book_id: i32,
     pub title: String,
     pub authors: String,
-    pub unit_price: f32,
+    pub category: String,
+    pub unit_price: f64,
 }
 
 pub fn load_books() -> Option<Vec<Book>> {

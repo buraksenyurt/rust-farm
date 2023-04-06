18f9d4aa8aa66b8899abb74409b43a2adca49a29
buraksenyurt <burakselimsenyurt@gmail.com>
2023-04-06 20:51:37 +0300

    The code is in trouble. Mockito in function is non-functional.

--- Practices/mockito_101/src/lib.rs
+++ Practices/mockito_101/src/lib.rs
@@ -8,24 +8,31 @@ Bunun testini yazarken servis sanki varmış gibi hareket edip fonksiyonu cover 
 
 mod business {
     use hyper::{Body, Client, Method, Request, StatusCode};
+    use mockito::ServerGuard;
     use serde::{Deserialize, Serialize};
 
     type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
-    pub async fn do_accounting(customer: &Customer) -> Result<BusinessResponse> {
+    pub async fn do_accounting(
+        customer: &Customer,
+        server: &ServerGuard,
+    ) -> Result<BusinessResponse> {
+        println!("{:?}",server.to_string());
         let body = serde_json::to_string(customer).unwrap();
+        let api_path = format!("{}/bank/api/checkLimit", server.url());
         let request = Request::builder()
             .method(Method::POST)
-            .uri("http://localhost:7777/bank/api/checkLimit")
+            .uri(api_path)
             .header("content-type", "application/json")
             .body(Body::from(body))?;
 
         let client = Client::new();
         let response = client.request(request).await?;
+        println!("{:?}", response.status());
         if response.status() == StatusCode::OK {
             // Limit müsaitse bazı işlemler yapılacak
             let body = hyper::body::to_bytes(response.into_body()).await?;
-            let result: DoAccountingResponse = serde_json::from_slice(&body).unwrap();
+            let result: DoAccountingResponse = serde_json::from_slice(&body)?;
             if result.code == 1 {
                 return Ok(BusinessResponse::new(
                     ReturnCode::Success,
@@ -39,7 +46,7 @@ mod business {
         } else {
             Ok(BusinessResponse::new(
                 ReturnCode::UnknownError,
-                "Bilinmeye hata!".to_string(),
+                "Bilinmeyen hata!".to_string(),
             ))
         }
     }
@@ -90,9 +97,19 @@ mod tests {
     use crate::business::{do_accounting, Customer, ReturnCode};
 
     #[tokio::test] // asenkron fonksiyonları test etmek için kullanılır
+                   //#[should_panic]
     async fn should_do_accounting_works() {
+        let mut server = mockito::Server::new();
+        println!("{:?}",server.to_string());
+        server
+            .mock("POST", "/bank/api/checkLimit")
+            .match_body(r#"{"code":1,"message":"müşteri limiti uygun"}"#)
+            .with_body("json")
+            .create_async()
+            .await;
+
         let cust = Customer::new(1230, "Sir Connery".to_string(), 1000.00);
-        let accounting_result = do_accounting(&cust).await.unwrap();
-        assert_eq!(accounting_result.return_code, ReturnCode::UnknownError);
+        let accounting_result = do_accounting(&cust, &server).await.unwrap();
+        assert_eq!(accounting_result.return_code, ReturnCode::Success);
     }
 }

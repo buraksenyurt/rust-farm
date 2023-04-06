5849deb7e2832a7352e83f2ced3e73c81c543bac
buraksenyurt <burakselimsenyurt@gmail.com>
2023-04-06 19:31:56 +0300

    Added some required structures.

--- Practices/mockito_101/src/lib.rs
+++ Practices/mockito_101/src/lib.rs
@@ -5,13 +5,14 @@ süreci şekillendiriyor.
 
 Bunun testini yazarken servis sanki varmış gibi hareket edip fonksiyonu cover etmek istiyoruz.
 */
+
 mod business {
     use hyper::{Body, Client, Method, Request, StatusCode};
-    use serde::Serialize;
+    use serde::{Deserialize, Serialize};
 
     type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
-    pub async fn do_accounting(customer: &Customer) -> Result<bool> {
+    pub async fn do_accounting(customer: &Customer) -> Result<BusinessResponse> {
         let body = serde_json::to_string(customer).unwrap();
         let request = Request::builder()
             .method(Method::POST)
@@ -23,10 +24,23 @@ mod business {
         let response = client.request(request).await?;
         if response.status() == StatusCode::OK {
             // Limit müsaitse bazı işlemler yapılacak
-            Ok(true)
+            let body = hyper::body::to_bytes(response.into_body()).await?;
+            let result: DoAccountingResponse = serde_json::from_slice(&body).unwrap();
+            if result.code == 1 {
+                return Ok(BusinessResponse::new(
+                    ReturnCode::Success,
+                    "İşleme uygun.".to_string(),
+                ));
+            }
+            Ok(BusinessResponse::new(
+                ReturnCode::LimitUnavailable,
+                "Limit yetersiz!".to_string(),
+            ))
         } else {
-            // Limit yetersiz gibi bir hata dönülebilir
-            Ok(false)
+            Ok(BusinessResponse::new(
+                ReturnCode::UnknownError,
+                "Bilinmeye hata!".to_string(),
+            ))
         }
     }
 
@@ -42,16 +56,43 @@ mod business {
             Self { id, title, balance }
         }
     }
+
+    #[derive(Deserialize)]
+    pub struct DoAccountingResponse {
+        pub code: i32,
+        pub message: String,
+    }
+
+    pub struct BusinessResponse {
+        pub return_code: ReturnCode,
+        pub message: String,
+    }
+
+    #[derive(Debug, PartialEq)]
+    pub enum ReturnCode {
+        Success,
+        LimitUnavailable,
+        UnknownError,
+    }
+
+    impl BusinessResponse {
+        pub fn new(return_code: ReturnCode, message: String) -> Self {
+            Self {
+                return_code,
+                message,
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::business::{do_accounting, Customer};
+    use crate::business::{do_accounting, Customer, ReturnCode};
 
-    #[tokio::test]
+    #[tokio::test] // asenkron fonksiyonları test etmek için kullanılır
     async fn should_do_accounting_works() {
         let cust = Customer::new(1230, "Sir Connery".to_string(), 1000.00);
         let accounting_result = do_accounting(&cust).await.unwrap();
-        assert_eq!(accounting_result, false);
+        assert_eq!(accounting_result.return_code, ReturnCode::UnknownError);
     }
 }

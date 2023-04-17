ecfc2bf5ee8079acee464c731c89f26a55535b92
buraksenyurt <burakselimsenyurt@gmail.com>
2023-04-17 15:35:26 +0300

    Skiped first line on source file.

--- Practices/csv_to_json/src/main.rs
+++ Practices/csv_to_json/src/main.rs
@@ -19,7 +19,13 @@ where
 pub fn convert() {
     let mut books: Vec<Book> = Vec::new();
     if let Ok(lines) = read_lines("library_samples.csv") {
+        let mut counter = 0;
         for line in lines {
+            if counter == 0 {
+                counter += 1;
+                continue;
+            }
+
             if let Ok(t) = line {
                 let parts = t.split('|').collect::<Vec<&str>>();
 

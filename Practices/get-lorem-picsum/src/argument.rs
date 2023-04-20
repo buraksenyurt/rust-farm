77f8f64e673fe71bd5119d1f8d4afec9f2a34d4f
buraksenyurt <burakselimsenyurt@gmail.com>
2023-04-20 14:32:13 +0300

    Added argument control structures.

---
+++ Practices/get-lorem-picsum/src/argument.rs
@@ -0,0 +1,35 @@
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq)]
+pub enum Command {
+    Many(List),
+    Single(u32),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct List {
+    pub page_number: u8,
+    pub count: u8,
+}
+
+impl List {
+    pub fn new(page_number: u8, count: u8) -> Self {
+        Self { page_number, count }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub enum CommandError {
+    Invalid,
+}
+impl FromStr for Command {
+    type Err = CommandError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "many" => Ok(Command::Many(List::new(1, 10))),
+            "single" => Ok(Command::Single(1)),
+            _ => Err(CommandError::Invalid),
+        }
+    }
+}

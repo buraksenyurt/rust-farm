e8f629b5b7fc2c8b8d3f59c8c574b74fb915191c
buraksenyurt <burakselimsenyurt@gmail.com>
2023-04-28 18:39:46 +0300

    Implemented server shut-down on ctrl+c

--- handson/hashdis/src/bin/server.rs
+++ handson/hashdis/src/bin/server.rs
@@ -3,34 +3,43 @@ use hashdis::processor::{Processor, ServerProcessor};
 use log::{error, info, warn};
 use std::io::Error;
 use tokio::net::TcpListener;
+use tokio::signal;
 
 #[tokio::main]
 pub async fn main() -> Result<(), Error> {
     env_logger::init();
-
-    let listener = TcpListener::bind("127.0.0.1:6380").await;
-    let processor = ServerProcessor;
-    match listener {
-        Ok(l) => {
-            info!("hashdis dinlemede. 127.0.0.1:6380");
-            let mut db = Db::default();
-            loop {
-                match l.accept().await {
-                    Ok((mut socket, addr)) => {
-                        warn!("{:?} {}", socket, addr);
-                        processor.run(&mut socket, Some(&mut db)).await?;
-                        continue;
-                    }
-                    Err(e) => {
-                        error!("{}", e);
-                        return Err(e);
-                    }
-                }
+    let mut listener = TcpListener::bind("127.0.0.1:6380")
+        .await
+        .expect("listener oluşturulamadı");
+    let shutdown = signal::ctrl_c();
+    tokio::select! {
+        result = run(&mut listener) => {
+            if let Err(e)=result{
+                error!("{}",e);
+                println!("{}",e);
             }
         }
-        Err(e) => {
-            error!("Sunucu hatası. {:?}", e);
-            Err(e)
+        _=shutdown => {
+            warn!("kapatılıyor");
+            println!(" kapatılıyor...");
+        }
+    }
+    Ok(())
+}
+
+pub async fn run(listener: &mut TcpListener) -> std::io::Result<()> {
+    let mut db = Db::default();
+    let processor = ServerProcessor;
+    loop {
+        match listener.accept().await {
+            Ok((mut socket, addr)) => {
+                info!("{:?} {}", socket, addr);
+                processor.run(&mut socket, Some(&mut db)).await?;
+                continue;
+            }
+            Err(e) => {
+                error!("{}", e);
+            }
         }
     }
 }

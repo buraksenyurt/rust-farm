7a484a8789acd509ee978678373e8bb2510b4607
buraksenyurt <burakselimsenyurt@gmail.com>
2023-05-27 21:47:51 +0300

    Refactored client code to receive server messages.

--- Practices/simple-udp-client/src/main.rs
+++ Practices/simple-udp-client/src/main.rs
@@ -6,16 +6,24 @@ fn main() {
         .connect("127.0.0.1:5001")
         .expect("UDP sunucusuna bağlanılamıyor");
     println!("Peer adresi {:?}", socket.peer_addr());
+    send_message(&socket, "Merhaba. Ben Dam.");
+    send_message(&socket, "Van Dam.");
+    send_message(&socket, "Claud Van Dam.");
+    send_message(&socket, "Jan Claud Van Dam.");
+}
+
+fn send_message(socket: &UdpSocket, message: &str) {
+    let mut buffer = [0; 1024];
+
     socket
-        .send("Merhaba. Ben Dam.".as_bytes())
+        .send(message.as_bytes())
         .expect("Mesaj gönderilemedi");
-    socket
-        .send("Van Dam.".as_bytes())
-        .expect("Mesaj gönderilemedi");
-    socket
-        .send("Claud Van Dam.".as_bytes())
-        .expect("Mesaj gönderilemedi");
-    socket
-        .send("Jan Claud Van Dam.".as_bytes())
-        .expect("Mesaj gönderilemedi");
+
+    match socket.recv_from(&mut buffer) {
+        Ok((size, _)) => {
+            let response = String::from_utf8_lossy(&buffer[..size]);
+            println!("Sunucudan der ki\n\t{}", response);
+        }
+        Err(_) => {}
+    }
 }

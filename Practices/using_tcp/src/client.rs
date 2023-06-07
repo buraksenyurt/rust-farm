5422c2de1f7945851292baac8917eeb9dc46b187
buraksenyurt <burakselimsenyurt@gmail.com>
2023-06-07 19:47:04 +0300

    Changed runtime for multi client.

--- Practices/using_tcp/src/client.rs
+++ Practices/using_tcp/src/client.rs
@@ -1,1 +1,34 @@
-fn main() {}
+use std::io::{stdin, Read, Write};
+use std::net::TcpStream;
+
+fn main() {
+    // Kullanıcdan sürekli girdi almak için sonsuz bir döngümüz var.
+    loop {
+        // Sunucuya bağlanıyoruz
+        match TcpStream::connect("localhost:3001") {
+            // Mesaj yollanabilecek bir stream varsa devam ediyor
+            Ok(mut stream) => {
+                println!("Sunucuya gönderilmek üzere bir mesaj yazın.\nÇıkmak için CTRL+C.");
+                let mut input = String::new();
+                stdin().read_line(&mut input).expect("Girdi okuma hatası");
+                // Stream'a terminalden gelen girdiyi basıyoruz
+                stream.write(input.trim().as_bytes()).unwrap();
+                // Bu kısımda da sunucudan gelen cevabı ele alıyoruz
+                let mut buffer = [0; 32];
+                // Sunucudan dönen içeriği buffer'a alıyoruz
+                stream.read(&mut buffer).unwrap();
+                // ve ekrana basıyoruz.
+                println!(
+                    "Echo from server:\t {:?}",
+                    std::str::from_utf8(&buffer)
+                        .unwrap()
+                        .trim_end_matches(char::from(0))
+                );
+            }
+            Err(e) => {
+                println!("Bağlantı hatası: {}", e);
+                continue;
+            }
+        };
+    }
+}
--- Practices/using_tcp/src/server.rs
+++ Practices/using_tcp/src/server.rs
@@ -1,19 +1,47 @@
 use std::io::{Read, Write};
-use std::net::TcpListener;
+use std::net::{TcpListener, TcpStream};
+use std::thread;
 
 fn main() {
     // localhost:3001 adresini dinleyecek şekilde bir nesne tanımlanır
     let listener = TcpListener::bind("127.0.0.1:3001").unwrap();
     println!("Sunucu dinliyor... localhost:3001");
+    // Birden fazla istemci bağlanacağını düşündüğümüzden
     for stream in listener.incoming() {
-        // Gelen stream'ler karşılama üzere mutable bir nesne oluşturuluyor
-        let mut stream = stream.unwrap();
-        // 32 byte'lık bloklar haline okuyabiliriz.
-        let mut buffer = [0; 32];
-        // gelen içeriği buffer'a alıyoruz
-        stream.read(&mut buffer).unwrap();
-        // Buffer içeriğini ekrana basıyoruz
-        println!("From Client:\t{}", String::from_utf8_lossy(&buffer));
-        stream.write(&mut buffer).unwrap();
+        match stream {
+            Ok(stream) => {
+                // her birin i ayrı ayrı ele alacak şekilde stream'leri birer thread'e açıyoruz.
+                thread::spawn(move || {
+                    handler(stream);
+                });
+            }
+            Err(e) => {
+                println!("Bir sorun var! {}", e);
+            }
+        }
+    }
+}
+
+// stream'leri ele ala iş parçacığı
+fn handler(mut stream: TcpStream) {
+    // 32 byte'lık içerik okuyacağız
+    let mut buffer = [0; 32];
+    loop {
+        match stream.read(&mut buffer) {
+            // Okunacak byte kalmayan kadar okuyoruz
+            Ok(bytes_read) => {
+                if bytes_read == 0 {
+                    break;
+                }
+                // İstemciden gelen mesajı ekrana basıyoruz
+                println!(
+                    "From Client:\t{}",
+                    String::from_utf8_lossy(&buffer[..bytes_read])
+                );
+                // Echo amaçlı olarak istemciden gelen mesajı kendisine yolluyoruz.
+                stream.write_all(&buffer[..bytes_read]).unwrap();
+            }
+            Err(_) => break,
+        }
     }
 }

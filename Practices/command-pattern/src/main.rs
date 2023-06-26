0d3016aad443340de558e493946986f23952a7e8
buraksenyurt <burakselimsenyurt@gmail.com>
2023-06-27 01:25:49 +0300

    Applied command design pattern.

--- Practices/command-pattern/src/main.rs
+++ Practices/command-pattern/src/main.rs
@@ -1,7 +1,52 @@
 use std::fmt::{Display, Formatter};
 
+/*
+   Senaryoda bir migration işlemi ele alınıyor.
+   Tablo oluşturma ve kolon ekleme gibi işleri belli bir sıra ile yapmak istiyoruz.
+   Command tasarım kalıbını bu senaryoya uyarlamaktayız.
+*/
 fn main() {
-    println!("Hello, world!");
+    let mut init = Plan::new();
+    let table = Table {
+        name: "Product".to_string(),
+    };
+    let cmd = Box::new(table);
+    init.add_command(cmd);
+
+    let cmd = Box::new(Column {
+        table: "Product".to_string(),
+        data_type: DataType::Text(10),
+        allow_null: false,
+        name: "title".to_string(),
+    });
+    init.add_command(cmd);
+
+    let cmd = Box::new(Column {
+        table: "Product".to_string(),
+        data_type: DataType::Decimal(8),
+        allow_null: false,
+        name: "unit_price".to_string(),
+    });
+    init.add_command(cmd);
+
+    let cmd = Box::new(Column {
+        table: "Product".to_string(),
+        data_type: DataType::Number(4),
+        allow_null: false,
+        name: "stock_size".to_string(),
+    });
+    init.add_command(cmd);
+
+    let cmd = Box::new(Column {
+        table: "Product".to_string(),
+        data_type: DataType::Bool,
+        allow_null: false,
+        name: "on_sale".to_string(),
+    });
+    init.add_command(cmd);
+
+    init.execute();
+    init.rollback();
 }
 
 enum ProcessResult {
@@ -31,7 +76,7 @@ impl Migration for Table {
 enum DataType {
     Text(u32),
     Number(u32),
-    Decimal(f32),
+    Decimal(u32),
     Bool,
 }
 impl Display for DataType {
@@ -53,7 +98,7 @@ impl Display for DataType {
     }
 }
 struct Column {
-    pub table: Table,
+    pub table: String,
     pub name: String,
     pub data_type: DataType,
     pub allow_null: bool,
@@ -63,7 +108,7 @@ impl Migration for Column {
     fn up(&self) -> ProcessResult {
         println!(
             "ALTER TABLE {} ADD COLUMN {} {} {}",
-            self.table.name,
+            self.table,
             self.name,
             self.data_type,
             if self.allow_null {
@@ -76,7 +121,29 @@ impl Migration for Column {
     }
 
     fn down(&self) -> ProcessResult {
-        println!("DROP TABLE {}", self.name);
+        println!("ALTER TABLE {} DROP COLUMN {}", self.table, self.name);
         ProcessResult::Completed
+    }
+}
+
+struct Plan {
+    commands: Vec<Box<dyn Migration>>,
+}
+
+impl Plan {
+    fn new() -> Self {
+        Self { commands: vec![] }
+    }
+
+    fn add_command(&mut self, cmd: Box<dyn Migration>) {
+        self.commands.push(cmd);
+    }
+
+    fn execute(&self) -> Vec<ProcessResult> {
+        self.commands.iter().map(|cmd| cmd.up()).collect()
+    }
+
+    fn rollback(&self) -> Vec<ProcessResult> {
+        self.commands.iter().rev().map(|cmd| cmd.down()).collect()
     }
 }

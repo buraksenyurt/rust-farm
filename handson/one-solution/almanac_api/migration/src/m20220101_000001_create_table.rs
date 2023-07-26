6f968c4223e7db5cf8e825e9ff81e87ca9a504e8
buraksenyurt <burakselimsenyurt@gmail.com>
2023-07-26 22:06:37 +0300

    Applied first migration.

---
+++ handson/one-solution/almanac_api/migration/src/m20220101_000001_create_table.rs
@@ -0,0 +1,44 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+
+        manager
+            .create_table(
+                Table::create()
+                    .table(Game::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Game::Id)
+                            .integer()
+                            .not_null()
+                            .auto_increment()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Game::Title).string().not_null())
+                    .col(ColumnDef::new(Game::Point).integer().not_null())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+
+        manager
+            .drop_table(Table::drop().table(Game::Table).to_owned())
+            .await
+    }
+}
+
+/// Learn more at https://docs.rs/sea-query#iden
+#[derive(Iden)]
+enum Game {
+    Table,
+    Id,
+    Title,
+    Point,
+}

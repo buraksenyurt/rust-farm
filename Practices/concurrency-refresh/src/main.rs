ee55198664ce411a3c3dc9cdff2ab0a8acec3c47
buraksenyurt <burakselimsenyurt@gmail.com>
2023-09-17 15:21:53 +0300

    Used move keyword for take the ownership into thread.

--- Practices/concurrency-refresh/src/main.rs
+++ Practices/concurrency-refresh/src/main.rs
@@ -1,20 +1,51 @@
-use std::thread;
-use std::thread::sleep;
+use std::f32::consts::PI;
+use std::thread::{sleep, spawn};
 use std::time::Duration;
 
 fn main() {
     //inital();
     //do_with_threads();
-    do_with_handles();
+    //do_with_handles();
+    // take_ownership_err();
+    take_ownership();
 }
+
+fn take_ownership() {
+    // 004
+    // 003'teki soruna istinaden move keyword'ü ile sahipliğin thread'a alınması denenebilir
+    let variable = PI;
+    let handle = spawn(move || {
+        for i in 0..10 {
+            println!("{}", i as f32 * variable);
+            sleep(Duration::from_millis(500));
+        }
+    });
+    handle.join();
+}
+
+// fn take_ownership_err() {
+//     // 003
+//     // thread içine dışardaki bir değişkenin sahipliği alınmak istenmiştir
+//     // ownership kurallarına göre bir değerin tek sahibi olabilir.
+//     // error[E0373]: closure may outlive the current function, but it borrows `variable`, which is owned by the current function
+//     // hatası oluşur
+//     let variable = PI;
+//     let handle = spawn(|| {
+//         for i in 0..10 {
+//             println!("{}", i as f32 * variable);
+//         }
+//     });
+//     handle.join();
+// }
 
 fn do_with_handles() {
     /*
+       002
        İlk döngüdeki thread'in bitmesi beklenir.
        Böylece ana thread'in diğer thread bitmeden sonlanması yani programın kapanması engellenir.
     */
 
-    let t_handle = thread::spawn(|| {
+    let t_handle = spawn(|| {
         for i in 0..10 {
             println!("İlk döngü. İş -> {i}");
             sleep(Duration::from_millis(1000));
@@ -31,13 +62,14 @@ fn do_with_handles() {
 
 fn do_with_threads() {
     /*
+       001
        İlk döngü ayrı bir thread içinde çalıştırılır.
        Problem şudur. İkinci döngü işleri birinci döngünün
        çalıştığı thread bitmeden biter.
        Main bu sebeple hemen sonlanır.
        Asnekron devam eden işlerin tamamlanmasını beklemek için handle nesneleri kullanılır.
     */
-    thread::spawn(|| {
+    spawn(|| {
         for i in 0..10 {
             println!("İlk döngü. İş -> {i}");
             sleep(Duration::from_millis(1000));
@@ -51,7 +83,7 @@ fn do_with_threads() {
 }
 
 fn inital() {
-    // Başlangıç Konumu
+    // Başlangıç Konumu 000
     // Tüm işler senkron çalışır
     // Önce birinci döngünün bitmesi beklenir
     // ardından ikinci döngü işleri devam eder

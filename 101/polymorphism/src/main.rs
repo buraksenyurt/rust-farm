5400152ab82423219afbb71b53226b81fe82541b
buraksenyurt <burakselimsenyurt@gmail.com>
2023-09-30 13:52:14 +0300

    Applied the super trait.

--- 101/polymorphism/src/main.rs
+++ 101/polymorphism/src/main.rs
@@ -1,6 +1,39 @@
 trait VehicleCapability {
     fn drive(&self);
 }
+
+// İstersek trait içerisinde varsayılan bir davranış da uyarlayabiliriz.
+// Bu durumda kendi veri modelimizde ezmezsek varsayılan versiyon çalışır
+trait VehicleCapability2 {
+    fn drive(&self) {
+        println!("Araç yolda");
+    }
+}
+
+trait WeaponCapability {
+    fn fire(&self) {
+        println!("Ateş ediyor");
+    }
+}
+
+// Military bir super trait'tir.
+// WeaponeCapability ile VehicleCapability2 trait'lerini de taşır
+trait Military: WeaponCapability + VehicleCapability2 {}
+struct Tank;
+// Tank veri modeli Military isimli super trait'i uyguladığı için
+// hem WeaponCapability hem de VehicleCapability2 trait'lerini
+// implemente etmek zorundadır.
+impl WeaponCapability for Tank {}
+impl VehicleCapability2 for Tank {}
+impl Military for Tank {}
+
+// Hem VehicleCapability hem WeaponCapability davranışlarını barındıran
+// Military davranışını uygulayan her veri modeli bu fonksiyona gelebilir.
+fn go_to_battle(vehicle: &impl Military) {
+    vehicle.drive();
+    vehicle.fire();
+}
+
 fn main() {
     let professor_zundap = FamilyCar;
     //my_car.drive();
@@ -12,6 +45,10 @@ fn main() {
     // static dispatch kullanımı
     go_trip_v2(&professor_zundap);
     go_trip_v2(&lightning_mcqueen);
+
+    // super trait kullanım örneği
+    let soldir_of_fortune = Tank;
+    go_to_battle(&soldir_of_fortune);
 }
 
 // dynamic dispatch

85e25afbea5ddd03dd7f2023bebd4cef1511cbe3
buraksenyurt <burakselimsenyurt@gmail.com>
2023-10-03 21:09:57 +0300

    Applied another approachs.

--- Practices/another_fizz_buzz/src/main.rs
+++ Practices/another_fizz_buzz/src/main.rs
@@ -2,8 +2,64 @@
    Fizz Buzz'ın farkl şekillerde çözümü
 */
 fn main() {
-    execute_v2();
+    //execute_v4();
+    execute_v3();
+    //execute_v2_point_5();
+    //execute_v2();
     // execute_v1();
+}
+
+fn execute_v4() {
+    // Version 4 - fonksiyonel yaklaşım
+    //
+    //
+    // let _ gibi bir atama yapmadan aşağıdakini kullanırsak
+    // note: iterators are lazy and do nothing unless consumed
+    // şeklinde bir uyarı mesajı alırız
+    // (1..=50).map(|num| match (num % 3 == 0, num % 5 == 0) {
+    //     (true, false) => println!("Fizz"),
+    //     (false, true) => println!("Buzz"),
+    //     (true, true) => println!("FizzBuzz"),
+    //     _ => println!("{num}"),
+    // });
+
+    (1..=50).for_each(|num| match (num % 3 == 0, num % 5 == 0) {
+        (true, false) => println!("Fizz"),
+        (false, true) => println!("Buzz"),
+        (true, true) => println!("FizzBuzz"),
+        _ => println!("{num}"),
+    });
+}
+
+fn execute_v3() {
+    // Version 3- pattern matching ile ama bu sefer String kullanımımız farklı
+    let mut output = String::new();
+    for num in 1..=50 {
+        match (num % 3 == 0, num % 5 == 0) {
+            (true, false) => output.push_str("Fizz"),
+            (false, true) => output.push_str("Buzz"),
+            (true, true) => output.push_str("FizzBuzz"),
+            _ => output.push_str(&*num.to_string()),
+        };
+        output.push('\n');
+    }
+    println!("{output}");
+}
+
+fn execute_v2_point_5() {
+    // Version 2.5 - pattern matching ile ama bu to_string kullanarak
+    // to_string bir çözümdür ama bilindiği üzere her to_string çağrısı
+    // bellekte yeni bir String allocation anlamına gelir.
+    for num in 1..=50 {
+        let result = match (num % 3 == 0, num % 5 == 0) {
+            (true, false) => "Fizz".to_string(),
+            (false, true) => "Buzz".to_string(),
+            (true, true) => "FizzBuzz".to_string(),
+            _ => num.to_string(),
+        };
+
+        println!("{result}");
+    }
 }
 
 fn execute_v2() {

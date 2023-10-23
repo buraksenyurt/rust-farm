248ef2bfe7fb911a67ad58e778cf0a42a11b0ee0
buraksenyurt <burakselimsenyurt@gmail.com>
2023-10-23 22:41:30 +0300

    Used feed-rs create.

--- handson/feed-eater/src/reader.rs
+++ handson/feed-eater/src/reader.rs
@@ -1,9 +1,26 @@
 use crate::feed::Feed;
 pub fn load_feeds(feeds: &Vec<Feed>) {
     feeds.iter().for_each(|f| {
-        println!("{}", f.title);
-
         let body = reqwest::blocking::get(f.url).unwrap().text().unwrap();
-        println!("{}", body);
+        // println!("{}", body);
+        let feed_body = feed_rs::parser::parse(body.as_bytes()).unwrap();
+        //println!("{:#?}", feed_body);
+        println!(
+            "{} ({}) - {}",
+            f.title,
+            feed_body.entries.len(),
+            feed_body.updated.unwrap_or_default()
+        );
+        feed_body.entries.iter().enumerate().for_each(|(idx, e)| {
+            println!(
+                "{idx} - {} {}",
+                e.title.clone().unwrap().content,
+                e.links
+                    .iter()
+                    .map(|link| link.href.clone())
+                    .collect::<Vec<String>>()
+                    .join(", ")
+            );
+        });
     });
 }

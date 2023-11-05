35741f5d25dfbae33ba6e00fdcc7b6c7c47b322b
buraksenyurt <burakselimsenyurt@gmail.com>
2023-11-05 17:11:27 +0300

    Added HTTP Get support.

--- handson/issue_tracker/src/main.rs
+++ handson/issue_tracker/src/main.rs
@@ -1,12 +1,16 @@
+use std::fmt::{Display, Formatter};
 use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
+const HTTP_OK: &str = "HTTP/1.1 200 OK";
+const HTTP_NOT_FOUND: &str = "HTTP/1.1 404 NOT FOUND";
+const CONTENT_TYPE: &str = "Content-Type: application/json";
 
 fn main() {
     let conn = Connection {
         host_address: "127.0.0.1:8086",
     };
     let listener = TcpListener::bind(conn.host_address).unwrap();
-    println!("Sunucu dinlemede {}", conn.host_address);
+    println!("Sunucu dinliyor: {}", conn.host_address);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
         conn.handle(stream);
@@ -20,18 +24,61 @@ struct Connection<'a> {
 impl<'a> Connection<'a> {
     fn handle(&self, mut stream: TcpStream) {
         let buffer_reader = BufReader::new(&mut stream);
-        let http_request: Vec<_> = buffer_reader
-            .lines()
-            .map(|result| result.unwrap())
-            .take_while(|line| !line.is_empty())
-            .collect();
+        let request_line = buffer_reader.lines().next().unwrap().unwrap();
+        println!("Talep -> {:#?}", request_line);
+        if request_line == "GET /hello HTTP/1.1" {
+            let response = Response::new(
+                HttpResponse::Ok,
+                "{\"response\":\"hello there\"}".to_string(),
+            );
+            stream.write_all(response.to_string().as_bytes()).unwrap();
+        } else if request_line == "GET /issues HTTP/1.1" {
+            let response =
+                Response::new(HttpResponse::Ok, "{\"issues\":\"issues list\"}".to_string());
+            stream.write_all(response.to_string().as_bytes()).unwrap();
+        } else {
+            println!("Geçerli bir talep değil!");
+            let response = Response::new(HttpResponse::NotFound, String::default());
+            stream.write_all(response.to_string().as_bytes()).unwrap();
+        }
+    }
+}
 
-        println!("Talep -> {:#?}", http_request);
+struct Response {
+    http_response: HttpResponse,
+    content: String,
+}
 
-        let status_line = "HTTP/1.1 200 OK\r\n\r\n";
-        let content = "{'response':'hello there'}";
-        let length = content.len();
-        let response=format!("{status_line}\r\nContent-Type: application/json\r\nContent-Length: {length}\r\n{content}\r\n");
-        stream.write_all(response.as_bytes()).unwrap();
+impl Response {
+    pub fn new(http_response: HttpResponse, content: String) -> Self {
+        Self {
+            http_response,
+            content,
+        }
+    }
+}
+
+impl Display for Response {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}{}\r\n\r\n{}",
+            self.http_response, CONTENT_TYPE, self.content
+        )
+    }
+}
+
+enum HttpResponse {
+    Ok,
+    NotFound,
+}
+
+impl Display for HttpResponse {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let output = match self {
+            HttpResponse::Ok => HTTP_OK,
+            HttpResponse::NotFound => HTTP_NOT_FOUND,
+        };
+        write!(f, "{}\r\n", output)
     }
 }

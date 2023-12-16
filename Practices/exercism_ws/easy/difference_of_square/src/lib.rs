d6559145620dd6feabbc44d91cc30c799c491148
buraksenyurt <burakselimsenyurt@gmail.com>
2023-12-16 16:04:18 +0300

    Completed the solution.

--- Practices/exercism_ws/easy/difference_of_square/src/lib.rs
+++ Practices/exercism_ws/easy/difference_of_square/src/lib.rs
@@ -1,14 +1,15 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    todo!("square of sum of 1...{n}")
+    (1..=n).sum::<u32>().pow(2)
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    todo!("sum of squares of 1...{n}")
+    (1..=n).fold(0, |total, number| total + number.pow(2))
 }
 
 pub fn difference(n: u32) -> u32 {
-    todo!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    square_of_sum(n) - sum_of_squares(n)
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;

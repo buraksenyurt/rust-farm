304a45a74f715031b37fa35ffa8957ebcca2d1c4
buraksenyurt <burakselimsenyurt@gmail.com>
2023-12-30 16:07:48 +0300

    Changed the message protocol.

--- Practices/psgre-lib/src/lib.rs
+++ Practices/psgre-lib/src/lib.rs
@@ -2,6 +2,8 @@ use std::io;
 use std::io::{Read, Write};
 use std::net::TcpStream;
 
+const PROTOCOL_VERSION: i32 = 196608; // PostgreSql Protocol version 3.0
+
 pub struct ConnectionSettings {
     pub host: String,
     pub port: u16,
@@ -16,20 +18,42 @@ pub enum ConnectionState {
     Failed(String),
 }
 
+fn create_handshake_message(conn: &ConnectionSettings) -> Vec<u8> {
+    let mut messages = Vec::new();
+
+    messages.extend_from_slice(&PROTOCOL_VERSION.to_be_bytes());
+    messages.extend_from_slice(b"user\0");
+    messages.extend_from_slice(conn.user.as_bytes());
+    messages.push(0);
+
+    messages.extend_from_slice(b"password\0");
+    messages.extend_from_slice(conn.password.as_bytes());
+    messages.push(0);
+
+    messages.extend_from_slice(b"database\0");
+    messages.extend_from_slice(conn.database.as_bytes());
+    messages.push(0);
+
+    messages.push(0);
+
+    let length = (messages.len() + 4) as i32;
+    let mut length_bytes = length.to_be_bytes().to_vec();
+    length_bytes.append(&mut messages);
+
+    length_bytes
+}
+
 pub fn contact(conn: &ConnectionSettings) -> io::Result<ConnectionState> {
     let address = format!("{}:{}", conn.host, conn.port);
     let mut stream = TcpStream::connect(address)?;
-    let contact_message = format!(
-        "user {}\0password {}\0database {}\0",
-        conn.user, conn.password, conn.database
-    );
-    stream.write_all(contact_message.as_bytes())?;
+    let contact_message = create_handshake_message(&conn);
+    stream.write_all(&contact_message)?;
     let mut response = [0; 1024];
     let bytes_read = stream.read(&mut response)?;
     if bytes_read > 0 {
         return Ok(ConnectionState::Connected(format!(
-            "Received: {:?}",
-            &response[..bytes_read]
+            "{}",
+            String::from_utf8(response[..bytes_read].to_vec()).unwrap()
         )));
     }
     Ok(ConnectionState::Failed(

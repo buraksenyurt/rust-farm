dd51a8c6764b76d084e1f478207724dddd978121
buraksenyurt <burakselimsenyurt@gmail.com>
2024-03-16 00:55:04 +0300

    Added another case, "Coin Change" problem.

--- Practices/bad-or-good/src/lib.rs
+++ Practices/bad-or-good/src/lib.rs
@@ -27,22 +27,103 @@ impl Fibonacci {
     }
 }
 
+pub struct CoinChange;
+
+impl CoinChange {
+    pub fn calc_worst(coins: &[i32], amount: i32) -> i32 {
+        if amount == 0 {
+            return 0;
+        }
+        let mut res = i32::MAX;
+        for &coin in coins {
+            if amount - coin >= 0 {
+                let sub_res = Self::calc_worst(coins, amount - coin);
+                if sub_res != i32::MAX && sub_res + 1 < res {
+                    res = sub_res + 1;
+                }
+            }
+        }
+        res
+    }
+
+    pub fn calc_green(coins: &[i32], amount: i32, memoization: &mut Vec<i32>) -> i32 {
+        if amount == 0 {
+            return 0;
+        }
+        if memoization[amount as usize] != -1 {
+            return memoization[amount as usize];
+        }
+        let mut result = i32::MAX;
+        for &coin in coins {
+            if amount - coin >= 0 {
+                let sub_res = Self::calc_green(coins, amount - coin, memoization);
+                if sub_res != i32::MAX && sub_res + 1 < result {
+                    result = sub_res + 1;
+                }
+            }
+        }
+        memoization[amount as usize] = result;
+        result
+    }
+}
+
+pub enum Coin {
+    Penny = 1,
+    Nickel = 5,
+    Dime = 10,
+    Quarter = 25,
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn calculate_fibonacci_in_worst() {
+    fn calculate_fibonacci_in_worst_test() {
         let actual = Fibonacci::calc_worst(32);
         let expected = 2_178_309;
         assert_eq!(actual, expected);
     }
 
     #[test]
-    fn calculate_fibonacci_in_green() {
+    fn calculate_fibonacci_in_green_test() {
         let mut memo_set: HashMap<u64, u64> = HashMap::new();
         let actual = Fibonacci::calc_green(51, &mut memo_set);
         let expected = 20_365_011_074;
         assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn calculate_coin_change_in_worst_test() {
+        /*
+           1 Penny = 1 Cent
+           1 Nickel = 5 Cents
+           1 Dime =  10 Cents
+           1 Quarter = 25 Cents
+        */
+        let coins = vec![
+            Coin::Penny as i32,
+            Coin::Nickel as i32,
+            Coin::Dime as i32,
+            Coin::Quarter as i32,
+        ];
+        let amount = 41;
+        let actual = CoinChange::calc_worst(&coins, amount);
+        assert_eq!(actual, 4); // 41 = 1 + 5 + 10 + 25 -> Toplam 4 adet bozuk para
+    }
+
+    #[test]
+    fn calculate_coin_change_in_green_text() {
+        let coins = vec![
+            Coin::Penny as i32,
+            Coin::Nickel as i32,
+            Coin::Dime as i32,
+            Coin::Quarter as i32,
+        ];
+        let amount = 41;
+        let mut memo_set = vec![-1; (amount + 1) as usize];
+        memo_set[0] = 0;
+        let result = CoinChange::calc_green(&coins, amount, &mut memo_set);
+        assert_eq!(result, 4); // 41 = 1 + 5 + 10 + 25 -> Toplam 4 adet bozuk para
     }
 }

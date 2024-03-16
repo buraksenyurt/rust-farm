a24c9d5401b1157c012b24b7a645563df0e3a808
buraksenyurt <burakselimsenyurt@gmail.com>
2024-03-16 22:05:34 +0300

    Seperated benchmarks.

--- Practices/bad-or-good/benches/coin_change_benchmark.rs
+++ Practices/bad-or-good/benches/coin_change_benchmark.rs
@@ -1,32 +1,6 @@
-use bad_or_good::{Coin, CoinChange, Fibonacci};
+use bad_or_good::{Coin, CoinChange};
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
-use std::collections::HashMap;
 use std::time::Duration;
-
-fn fibonacci_benchmarks(c: &mut Criterion) {
-    let mut worst_case_group = c.benchmark_group("Fibonacci Worst Case");
-    worst_case_group.measurement_time(Duration::from_secs(20));
-    worst_case_group.sample_size(10);
-
-    for i in 36..=40 {
-        worst_case_group.bench_with_input(BenchmarkId::new("Worst", i), &i, |b, &i| {
-            b.iter(|| Fibonacci::calc_worst(black_box(i)));
-        });
-    }
-    worst_case_group.finish();
-
-    let mut green_case_group = c.benchmark_group("Fibonacci Green Case");
-    green_case_group.measurement_time(Duration::from_secs(10));
-    green_case_group.sample_size(10);
-
-    for i in 36..=40 {
-        green_case_group.bench_with_input(BenchmarkId::new("Green", i), &i, |b, &i| {
-            let mut memo_set: HashMap<u64, u64> = HashMap::new();
-            b.iter(|| Fibonacci::calc_green(black_box(i), black_box(&mut memo_set)));
-        });
-    }
-    green_case_group.finish();
-}
 
 fn coin_change_benchmarks(c: &mut Criterion) {
     let mut worst_case_group = c.benchmark_group("Coin Change Worst Case");
@@ -39,7 +13,7 @@ fn coin_change_benchmarks(c: &mut Criterion) {
         Coin::Quarter as i32,
     ];
 
-    for amount in 36..=40 {
+    for amount in [1, 17, 41, 63, 77] {
         worst_case_group.bench_with_input(BenchmarkId::new("Worst", amount), &amount, |b, &i| {
             b.iter(|| CoinChange::calc_worst(black_box(&coins), black_box(i)));
         });
@@ -50,7 +24,7 @@ fn coin_change_benchmarks(c: &mut Criterion) {
     green_case_group.measurement_time(Duration::from_secs(10));
     green_case_group.sample_size(10);
 
-    for amount in 36..=40 {
+    for amount in [1, 17, 41, 63, 77] {
         green_case_group.bench_with_input(BenchmarkId::new("Green", amount), &amount, |b, &i| {
             let mut memo_set = vec![-1; (i + 1) as usize];
             memo_set[0] = 0;
---
+++ Practices/bad-or-good/benches/fibonacci_benchmark.rs
@@ -0,0 +1,36 @@
+use bad_or_good::Fibonacci;
+use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
+use std::collections::HashMap;
+use std::time::Duration;
+
+fn fibonacci_benchmarks(c: &mut Criterion) {
+    let mut worst_case_group = c.benchmark_group("Fibonacci Worst Case");
+    worst_case_group.measurement_time(Duration::from_secs(20));
+    worst_case_group.sample_size(10);
+
+    for i in 36..=40 {
+        worst_case_group.bench_with_input(BenchmarkId::new("Worst", i), &i, |b, &i| {
+            b.iter(|| Fibonacci::calc_worst(black_box(i)));
+        });
+    }
+    worst_case_group.finish();
+
+    let mut green_case_group = c.benchmark_group("Fibonacci Green Case");
+    green_case_group.measurement_time(Duration::from_secs(10));
+    green_case_group.sample_size(10);
+
+    for i in 36..=40 {
+        green_case_group.bench_with_input(BenchmarkId::new("Green", i), &i, |b, &i| {
+            let mut memo_set: HashMap<u64, u64> = HashMap::new();
+            b.iter(|| Fibonacci::calc_green(black_box(i), black_box(&mut memo_set)));
+        });
+    }
+    green_case_group.finish();
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default().sample_size(10);
+    targets = fibonacci_benchmarks
+}
+criterion_main!(benches);

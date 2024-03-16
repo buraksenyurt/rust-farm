dc265311760cfb24e23dd2cdd789429b760d17c3
buraksenyurt <burakselimsenyurt@gmail.com>
2024-03-16 18:30:41 +0300

    Updated benchmark for Coin Change problem.

--- Practices/bad-or-good/benches/green_coding_benchmark.rs
+++ Practices/bad-or-good/benches/green_coding_benchmark.rs
@@ -1,10 +1,10 @@
-use bad_or_good::Fibonacci;
+use bad_or_good::{Coin, CoinChange, Fibonacci};
 use criterion::{black_box, criterion_group, criterion_main, BenchmarkId, Criterion};
 use std::collections::HashMap;
 use std::time::Duration;
 
 fn fibonacci_benchmarks(c: &mut Criterion) {
-    let mut worst_case_group = c.benchmark_group("Worst Case");
+    let mut worst_case_group = c.benchmark_group("Fibonacci Worst Case");
     worst_case_group.measurement_time(Duration::from_secs(20));
     worst_case_group.sample_size(10);
 
@@ -15,8 +15,8 @@ fn fibonacci_benchmarks(c: &mut Criterion) {
     }
     worst_case_group.finish();
 
-    let mut green_case_group = c.benchmark_group("Green Case");
-    green_case_group.measurement_time(Duration::from_secs(20));
+    let mut green_case_group = c.benchmark_group("Fibonacci Green Case");
+    green_case_group.measurement_time(Duration::from_secs(10));
     green_case_group.sample_size(10);
 
     for i in 36..=40 {
@@ -28,9 +28,43 @@ fn fibonacci_benchmarks(c: &mut Criterion) {
     green_case_group.finish();
 }
 
+fn coin_change_benchmarks(c: &mut Criterion) {
+    let mut worst_case_group = c.benchmark_group("Coin Change Worst Case");
+    worst_case_group.measurement_time(Duration::from_secs(20));
+    worst_case_group.sample_size(10);
+    let coins = vec![
+        Coin::Penny as i32,
+        Coin::Nickel as i32,
+        Coin::Dime as i32,
+        Coin::Quarter as i32,
+    ];
+
+    for amount in 36..=40 {
+        worst_case_group.bench_with_input(BenchmarkId::new("Worst", amount), &amount, |b, &i| {
+            b.iter(|| CoinChange::calc_worst(black_box(&coins), black_box(i)));
+        });
+    }
+    worst_case_group.finish();
+
+    let mut green_case_group = c.benchmark_group("Coin Change Green Case");
+    green_case_group.measurement_time(Duration::from_secs(10));
+    green_case_group.sample_size(10);
+
+    for amount in 36..=40 {
+        green_case_group.bench_with_input(BenchmarkId::new("Green", amount), &amount, |b, &i| {
+            let mut memo_set = vec![-1; (i + 1) as usize];
+            memo_set[0] = 0;
+            b.iter(|| {
+                CoinChange::calc_green(black_box(&coins), black_box(i), black_box(&mut memo_set))
+            });
+        });
+    }
+    green_case_group.finish();
+}
+
 criterion_group! {
     name = benches;
     config = Criterion::default().sample_size(10);
-    targets = fibonacci_benchmarks
+    targets = coin_change_benchmarks
 }
 criterion_main!(benches);

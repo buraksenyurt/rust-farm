b157b7237ca98f21a52d71b4555ed89a40b587b1
buraksenyurt <burakselimsenyurt@gmail.com>
2024-03-18 13:23:36 +0300

    Added a new case

--- Practices/bad-or-good/src/lib.rs
+++ Practices/bad-or-good/src/lib.rs
@@ -74,6 +74,29 @@ pub enum Coin {
     Quarter = 25,
 }
 
+pub struct StringOps;
+
+impl StringOps {
+    pub fn calc_worst(words: Vec<String>) -> String {
+        let mut output = String::new();
+        for word in words {
+            output += &word;
+        }
+        output
+    }
+
+    pub fn calc_green(words: Vec<String>) -> String {
+        // Önce toplam uzunluğu bulalım.
+        let len = words.iter().map(|w| w.len()).sum();
+        // Bulunun uzunluk kadar boyuta sahip bir String tanımlayalım
+        let mut output = String::with_capacity(len);
+        for word in words {
+            output.push_str(&word);
+        }
+        output
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -113,7 +136,7 @@ mod tests {
     }
 
     #[test]
-    fn calculate_coin_change_in_green_text() {
+    fn calculate_coin_change_in_green_test() {
         let coins = vec![
             Coin::Penny as i32,
             Coin::Nickel as i32,
@@ -125,5 +148,41 @@ mod tests {
         memo_set[0] = 0;
         let result = CoinChange::calc_green(&coins, amount, &mut memo_set);
         assert_eq!(result, 4); // 41 = 1 + 5 + 10 + 25 -> Toplam 4 adet bozuk para
+    }
+
+    #[test]
+    fn concatenate_string_in_worst_test() {
+        let words = vec![
+            "C#".to_string(),
+            "Python".to_string(),
+            "Go".to_string(),
+            "Rust".to_string(),
+            "C++".to_string(),
+            "C".to_string(),
+            "Assembly".to_string(),
+            "Cobol".to_string(),
+            "Delphi".to_string(),
+        ];
+        let actual = StringOps::calc_worst(words);
+        let expected = "C#PythonGoRustC++CAssemblyCobolDelphi".to_string();
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn concatenate_string_in_green_test() {
+        let words = vec![
+            "C#".to_string(),
+            "Python".to_string(),
+            "Go".to_string(),
+            "Rust".to_string(),
+            "C++".to_string(),
+            "C".to_string(),
+            "Assembly".to_string(),
+            "Cobol".to_string(),
+            "Delphi".to_string(),
+        ];
+        let actual = StringOps::calc_green(words);
+        let expected = "C#PythonGoRustC++CAssemblyCobolDelphi".to_string();
+        assert_eq!(actual, expected);
     }
 }

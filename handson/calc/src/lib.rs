46e6ef7d65894df64dca2dec007d55ba6350791d
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2024-04-16 06:27:34 +0000

    Added unit tests.

--- handson/calc/src/lib.rs
+++ handson/calc/src/lib.rs
@@ -1,5 +1,27 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn add(x: f64, y: f64) -> f64 {
+    x + y
+}
+
+#[wasm_bindgen]
+pub fn subtract(x: f64, y: f64) -> f64 {
+    x - y
+}
+
+#[wasm_bindgen]
+pub fn multiply(x: f64, y: f64) -> f64 {
+    x * y
+}
+
+#[wasm_bindgen]
+pub fn divide(x: f64, y: f64) -> f64 {
+    if y == 0.0 {
+        panic!("Division by zero!");
+    }
+    x / y
 }
 
 #[cfg(test)]
@@ -7,8 +29,33 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn add_two_float_test() {
+        let result = add(2.2, 2.2);
+        assert_eq!(result, 4.4);
+    }
+
+    #[test]
+    fn multiply_two_float_test() {
+        let result = multiply(2.2, 2.0);
+        assert_eq!(result, 4.4);
+    }
+
+    #[test]
+    fn subtract_two_float_test() {
+        let result = subtract(4.2,4.2);
+        assert_eq!(result, 0.0);
+    }
+
+    #[test]
+    fn divide_two_float_test() {
+        let result = divide(2.2, 2.0);
+        assert_eq!(result, 1.1);
+    }
+
+    #[test]
+    #[should_panic]
+    fn divide_two_float_panics_test() {
+        let result = divide(2.2, 0.0);
+        assert_eq!(result, 1.1);
     }
 }

08d47227b94ec8c76950c3315101390cdd45c573
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2024-04-16 08:29:58 +0000

    Applied server side.

---
+++ handson/calc/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 4445;
+
+app.use(express.static('public'));
+app.use('/pkg', express.static('pkg')); 
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});

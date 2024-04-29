5f48b35e1162bdb3a5f7e59bc52819c7191083f1
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2024-04-29 11:27:10 +0000

    Moved frontend state logic into lib

--- handson/states-on-wasm/src/lib.rs
+++ handson/states-on-wasm/src/lib.rs
@@ -1,4 +1,6 @@
 use wasm_bindgen::prelude::*;
+use web_sys::window;
+use web_sys::HtmlElement;
 
 #[wasm_bindgen]
 pub struct Game {
@@ -14,10 +16,12 @@ enum GameState {
 
 #[wasm_bindgen]
 impl Game {
-    pub fn new() -> Game {
-        Game {
+    pub fn new() -> Self {
+        let game = Game {
             state: GameState::Menu,
-        }
+        };
+        game.update_visibility();
+        game
     }
 
     pub fn update_state(&mut self, new_state: &str) {
@@ -29,5 +33,47 @@ impl Game {
                 return;
             }
         };
+        self.update_visibility();
+    }
+
+    fn update_visibility(&self) {
+        let document = window().unwrap().document().unwrap();
+        let menu = document
+            .get_element_by_id("divMenu")
+            .unwrap()
+            .dyn_into::<HtmlElement>()
+            .unwrap();
+        let game = document
+            .get_element_by_id("divGame")
+            .unwrap()
+            .dyn_into::<HtmlElement>()
+            .unwrap();
+        let credits = document
+            .get_element_by_id("divCredits")
+            .unwrap()
+            .dyn_into::<HtmlElement>()
+            .unwrap();
+
+        menu.style().set_property("display", "none").unwrap();
+        game.style().set_property("display", "none").unwrap();
+        credits.style().set_property("display", "none").unwrap();
+
+        match self.state {
+            GameState::Menu => {
+                menu.style().set_property("display", "block").unwrap();
+            }
+            GameState::Playing => {
+                game.style().set_property("display", "block").unwrap();
+            }
+            GameState::Credits => {
+                credits.style().set_property("display", "block").unwrap();
+            }
+        }
+    }
+}
+
+impl Default for Game {
+    fn default() -> Self {
+        Self::new()
     }
 }

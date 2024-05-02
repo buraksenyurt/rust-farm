1c39e9b44f60286a1958246a4ac5478c8c07f8c1
Burak Selim Senyurt <burakselimsenyurt@gmail.com>
2024-05-02 10:16:37 +0000

    Refactored work item structure

--- handson/can-ban/src/lib.rs
+++ handson/can-ban/src/lib.rs
@@ -4,9 +4,10 @@ use wasm_bindgen::prelude::*;
 pub struct WorkItem {
     id: u32,
     title: String,
-    duration: u32,
-    duration_type: DurationType,
-    size:Size
+    duration: Option<u32>,
+    duration_type: Option<DurationType>,
+    size: Option<Size>,
+    status: Status,
 }
 
 #[wasm_bindgen]
@@ -25,13 +26,47 @@ pub enum Size {
     Epic,
 }
 
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
+#[derive(Debug, PartialEq, Clone)]
+#[wasm_bindgen]
+pub enum Status {
+    Todo,
+    Inprogress,
+    Completed,
+}
 
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
+impl WorkItem {
+    pub fn new(id: u32, title: String) -> Self {
+        Self {
+            id,
+            title,
+            duration: None,
+            duration_type: None,
+            size: None,
+            status: Status::Todo,
+        }
+    }
+    pub fn get_current_status(&self) -> Status {
+        self.status.clone()
+    }
+    pub fn change_status(&mut self, new_status: Status) {
+        self.status = new_status
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_work_item_status_must_at_todo_state_test() {
+        let work_item = WorkItem::new(1, "Design the system".to_string());
+        assert_eq!(work_item.get_current_status(), Status::Todo);
+    }
+
+    #[test]
+    fn change_status_from_test() {
+        let mut work_item = WorkItem::new(1, "Design the system".to_string());
+        work_item.change_status(Status::Inprogress);
+        assert_eq!(work_item.get_current_status(), Status::Inprogress);
+    }
+}

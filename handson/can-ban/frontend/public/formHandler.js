4f45556d012a712c7cf89336a6778ff661cde5bd
buraksenyurt <burakselimsenyurt@gmail.com>
2024-05-18 23:00:55 +0300

    Added some form validations.

--- handson/can-ban/frontend/public/formHandler.js
+++ handson/can-ban/frontend/public/formHandler.js
@@ -6,11 +6,31 @@ export function setupFormListener() {
     form.addEventListener('submit', async (event) => {
         event.preventDefault();
         const formData = new FormData(form);
-        console.log(formData);
+        // console.log(formData);
         const title = formData.get('inputTitle');
         const duration = formData.get('inputDuration');
         const durationType = formData.get('inputDurationType');
         const size = formData.get('inputSize');
+
+        if (title.length < 10 || title.length > 100) {
+            showAlert('Title must be between 10 and 100 characters long', 'danger');
+            return;
+        }
+
+        if (!duration) {
+            showAlert('Please give me an estimated duration', 'danger');
+            return;
+        }
+
+        if (!durationType) {
+            showAlert('Duration type must be selected', 'danger');
+            return;
+        }
+
+        if (!size) {
+            showAlert('Size must be selected', 'danger');
+            return;
+        }
 
         try {
             const itemsCount = await getWorkItemsCount();

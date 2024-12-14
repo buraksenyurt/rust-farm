03e6977bc5d72669187a47121736c213f904dd77
buraksenyurt <burakselimsenyurt@gmail.com>
2024-12-14 20:17:12 +0300

    Refactored code

--- handson/teraports/src/ds/csv_ds.rs
+++ handson/teraports/src/ds/csv_ds.rs
@@ -1,13 +1,12 @@
 use crate::ds::TabularLoader;
 use serde::de::DeserializeOwned;
-use serde::Serialize;
 
 pub struct CsvDataSource {
     pub file_name: String,
 }
 
 impl TabularLoader for CsvDataSource {
-    async fn load_data<T: Serialize + DeserializeOwned + Send>(&self) -> Vec<T> {
+    async fn load_data<T: DeserializeOwned>(&self) -> Vec<T> {
         let content = tokio::fs::read_to_string(&self.file_name)
             .await
             .expect("Error reading tabular data file");
--- handson/teraports/src/ds/mod.rs
+++ handson/teraports/src/ds/mod.rs
@@ -5,12 +5,11 @@ pub use csv_ds::*;
 pub use json_ds::*;
 
 use serde::de::DeserializeOwned;
-use serde::Serialize;
 
 pub trait JsonLoader {
     async fn load_data<T: DeserializeOwned>(&self) -> T;
 }
 
 pub trait TabularLoader {
-    async fn load_data<T: Serialize + DeserializeOwned + Send>(&self) -> Vec<T>;
+    async fn load_data<T: DeserializeOwned>(&self) -> Vec<T>;
 }
